3a6cdfcb2bbfafeefb92f65e297c95d6e6e6aa86
La Quoc Anh <anh.lq@trungthuy.vn>
2021-09-16 22:07:23 +0700

    fix(web-landing): escape character bug

--- web-landing/src/components/UI/404/index.tsx
+++ web-landing/src/components/UI/404/index.tsx
@@ -24,7 +24,7 @@ const NotFoundUI = ({}: NotFoundUIProps) => {
                     <Img src="/logo/logo192.png" h="full" />
                 </Box>
                 <MyText fontSize={["xl", "2xl"]} textTransform="uppercase" color="main.yellow" fontWeight="bold" mb={4}>
-                    Look like you've lost!
+                    {`Look like you've lost!`}
                 </MyText>
                 <SpecialButton text="Home" onClick={() => router.push("/")} />
             </Flex>
