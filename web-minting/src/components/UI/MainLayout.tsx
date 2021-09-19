1731e097f14ecae6ca40530e24ef33f5c42c409d
La Quoc Anh <anh.lq@trungthuy.vn>
2021-09-19 10:35:14 +0700

    fix(web-minting): main layout

--- web-minting/src/components/UI/MainLayout.tsx
+++ web-minting/src/components/UI/MainLayout.tsx
@@ -1,40 +1,38 @@
-import { Flex } from "@chakra-ui/layout";
-import { useMetamask } from "@hooks/useMetamask";
-import { useRouter } from "next/router";
-import React, { useEffect } from "react";
-import { NavBar } from "../shared/NavBar";
+import { Flex } from "@chakra-ui/layout"
+import { useMetamask } from "@hooks/useMetamask"
+import { useRouter } from "next/router"
+import React, { useEffect } from "react"
+import { NavBar } from "../shared/NavBar"
 
 interface Props {
-	children: React.ReactNode;
+    children: React.ReactNode
 }
 
 const MainLayout = ({ children }: Props) => {
-	const { metaState } = useMetamask();
-	const router = useRouter();
+    const { metaState } = useMetamask()
+    const router = useRouter()
 
-	useEffect(() => {
-		if (!metaState.isConnected) {
-			router.push("/");
-		}
-	}, [metaState.isConnected, router]);
+    useEffect(() => {
+        if (!metaState.isConnected) {
+            router.push("/")
+        }
+    }, [metaState.isConnected, router])
 
-	return (
-		metaState.isConnected && (
-			<Flex
-				color="whiteAlpha.900"
-				bg="url(/images/bgMinting.png) no-repeat"
-				bgSize="100% 100%"
-				w="full"
-				h="100vh"
-				flexDir="column"
-				pos="relative"
-			>
-				<NavBar />
-				<Flex flex={1} overflow="hidden">
-					{children}
-				</Flex>
-			</Flex>
-		)
-	);
-};
-export default MainLayout;
+    return (
+        <Flex
+            color="whiteAlpha.900"
+            bg="url(/images/bgMinting.png) no-repeat"
+            bgSize="100% 100%"
+            w="full"
+            h="100vh"
+            flexDir="column"
+            pos="relative"
+        >
+            <NavBar />
+            <Flex flex={1} overflow="hidden">
+                {children}
+            </Flex>
+        </Flex>
+    )
+}
+export default MainLayout
