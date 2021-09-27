1af8b9bb980b1a2400aae7395faa85ce6c6606b5
sieutihons10 <sieutihons10@gmail.com>
2021-09-27 11:57:07 +0700

    update

--- web-minting/src/api/nft.ts
+++ web-minting/src/api/nft.ts
@@ -1,7 +1,7 @@
 import config from "./config"
 import axios from "axios"
 
-export const getListNFT = async (publicAddress, from, to, type) => {
+export const getListNFT = async (publicAddress: string, from: number, to: number, type: string) => {
 	const { data } = await axios.get(
 		`/nft/get-list-nft?publicAddress=${publicAddress}&min=${from}&max=${to}&type=${type}`,
 		config
@@ -14,19 +14,25 @@ export const getListNFT = async (publicAddress, from, to, type) => {
 }
 
 //get info nft by id
-export const getInfoNFT = async (publicAddress, id, type) => {
+export const getInfoNFT = async (publicAddress: string, id: number | string, type: string) => {
 	const { data } = await axios.get(`/nft/get-nft?publicAddress=${publicAddress}&id=${id}&race=${type}`, config)
 
 	return data.message
 }
 
-export const getMerkle = async (type,id) => {
-	const { data } = await axios.get(`/${type.toLowerCase()}-sc/merkle/${id}`, config)
+export const getMerkle = async (id: number | string, type: string) => {
+	const { data } = await axios.get(`/${type}-sc/merkle/${id}`, config)
 	return data
 }
 
 //change emotion
-export const changeEmotion = async (accessToken, id, emotion, publicAddress, race) => {
+export const changeEmotion = async (
+	accessToken: string,
+	id: number | string,
+	emotion: string,
+	publicAddress: string,
+	race: string
+) => {
 	const { data } = await axios.post(
 		`/nft/change-emotion`,
 		{
--- web-minting/src/components/UI/Inventory/PopupModal.tsx
+++ web-minting/src/components/UI/Inventory/PopupModal.tsx
@@ -74,18 +74,22 @@ const PopupModal = ({ selectId }: PopupProps) => {
 		}
 	)
 
-	const { data: merkle } = useQuery(["MerkleId", selectId], () => getMerkle(selectId.id, selectId.race), {
-		onError: (error) => {
-			console.log(error)
-		},
-	})
+	const { data: merkle } = useQuery(
+		["MerkleId", selectId],
+		() => getMerkle(selectId.id, selectId.race.toLowerCase()),
+		{
+			onError: (error) => {
+				console.log(error)
+			},
+		}
+	)
 
 	const { mutate } = useMutation(
 		() => changeEmotion(metaState.accessToken, selectId.id, currentEmotion, metaState.accountLogin, selectId.race),
 		{
 			onSuccess: () => {
 				queryClient.invalidateQueries(["NFTId", selectId])
-				queryClient.invalidateQueries(["NFT"])
+				queryClient.invalidateQueries([`${selectId.race}-NFT`])
 				toast("success", "Change emotion successfully")
 			},
 			onError: () => {},
