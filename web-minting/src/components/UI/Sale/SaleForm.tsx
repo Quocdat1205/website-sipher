e7bc59ae150baecea89f7a6f25a458adedbd7ee8
sieutihons10 <sieutihons10@gmail.com>
2021-09-30 13:36:49 +0700

    update SC new

--- web-minting/src/components/UI/Sale/SaleForm.tsx
+++ web-minting/src/components/UI/Sale/SaleForm.tsx
@@ -11,197 +11,198 @@ import { getMetamaskBalance } from "src/helper/metamask"
 import Counter from "@components/UI/Sale/Counter"
 
 interface SaleFormProps {
-    mode: "private" | "public"
+	mode: "private" | "public"
 }
 
 const SaleForm = ({ mode }: SaleFormProps) => {
-    const { metaState, toast, saleTime } = useWalletContext()
-    const queryClient = useQueryClient()
-    const [isLoadingBtn, setIsLoadingBtn] = useState(false)
-    const [currentPrice, setCurrentPrice] = useState(0.1)
-    const [slot, setSlot] = useState(0)
+	const { metaState, toast, saleTime } = useWalletContext()
+	const queryClient = useQueryClient()
+	const [isLoadingBtn, setIsLoadingBtn] = useState(false)
+	const [currentPrice, setCurrentPrice] = useState(0.1)
+	const [slot, setSlot] = useState(0)
 
-    const handlePriceChange = useCallback((value: number) => setCurrentPrice(value), [])
+	const handlePriceChange = useCallback((value: number) => setCurrentPrice(value), [])
 
-    const getCurrentPrice = async () => {
-        let price = 0
-        if (mode === "public") {
-            price = await getPublicCurrentPrice()
-        } else price = 0.1
-        return price
-    }
-    const { data: userRecord, isLoading: isLoadingRecord } = useQuery("user-record", () =>
-        getUserRecord(metaState.accountLogin)
-    )
-    const calculateSlotPrice = () => {
-        return parseFloat((slot * currentPrice).toFixed(2))
-    }
-    const mint = async () => {
-        if (mode === "public") {
-            if (userRecord && userRecord.publicBought >= PUBLIC_CAP) {
-                toast({ status: "error", title: "Confirmation error!", message: "You can only buy up to 5 NFTs" })
-                return
-            }
-        } else if (userRecord && userRecord.whitelistBought >= metaState.isWhitelisted.cap) {
-            toast({
-                status: "error",
-                title: "Confirmation error!",
-                message: `You can only buy up to ${metaState.isWhitelisted.cap} NFTs`,
-            })
-            return
-        }
+	const getCurrentPrice = async () => {
+		let price = 0
+		if (mode === "public") {
+			price = await getPublicCurrentPrice()
+		} else price = 0.1
+		return price
+	}
+	const { data: userRecord, isLoading: isLoadingRecord } = useQuery("user-record", () =>
+		getUserRecord(metaState.accountLogin)
+	)
+	const calculateSlotPrice = () => {
+		return parseFloat((slot * currentPrice).toFixed(2))
+	}
+	const mint = async () => {
+		if (mode === "public") {
+			if (userRecord && userRecord.publicBought >= PUBLIC_CAP) {
+				toast({ status: "error", title: "Confirmation error!", message: "You can only buy up to 5 NFTs" })
+				return
+			}
+		} else if (userRecord && userRecord.whitelistBought >= metaState.isWhitelisted.cap) {
+			toast({
+				status: "error",
+				title: "Confirmation error!",
+				message: `You can only buy up to ${metaState.isWhitelisted.cap} NFTs`,
+			})
+			return
+		}
 
-        let currentPrice = await getCurrentPrice()
-        let slotPrice = parseFloat((slot * currentPrice).toFixed(2))
-        const {
-            accountLogin: address,
-            isWhitelisted: { cap, proof },
-        } = metaState
-        await sendSmartContract({ address, slot, slotPrice, cap, proof })
-        toast({ status: "success", title: "Transaction created successfully!", duration: 6000 })
-        setSlot(0)
-        queryClient.invalidateQueries("total-supply")
-        queryClient.invalidateQueries("user-record")
-    }
+		let currentPrice = await getCurrentPrice()
+		let slotPrice = parseFloat((slot * currentPrice).toFixed(2))
+		const {
+			accountLogin: address,
+			isWhitelisted: { cap, proof },
+		} = metaState
+		await sendSmartContract({ address, slot, slotPrice, cap, proof })
+		toast({ status: "success", title: "Transaction created successfully!", duration: 6000 })
+		setSlot(0)
+		queryClient.invalidateQueries("total-supply")
+		queryClient.invalidateQueries("user-record")
+	}
 
-    const handleConfirm = async () => {
-        if (metaState.chain?.id !== CHAIN_ID) {
-            toast({ status: "error", title: "Wrong ethereum network!", message: "Please switch to Ethereum Mainnet." })
-            return
-        }
-        if (metaState.status[mode] !== "SALE") {
-            toast({ status: "error", title: "NFT is not on sale!", message: "Please comeback later." })
-            return
-        }
-        const balance = await getMetamaskBalance(metaState.accountLogin)
-        if (balance < calculateSlotPrice()) {
-            toast({ status: "error", title: "Insufficient balance!" })
-            return
-        }
-        let checkSC = await checkSmartContract(metaState.accountLogin)
-        if (!checkSC) {
-            toast({ status: "error", title: "Failed to check smart contract!" })
-            return
-        }
-        try {
-            setIsLoadingBtn(true)
-            await mint()
-            setIsLoadingBtn(false)
-            queryClient.invalidateQueries("total-supply")
-            queryClient.invalidateQueries("user-record")
-        } catch (error) {
-            toast({ status: "error", title: "Something went wrong!", message: "Try again later." })
-            setIsLoadingBtn(false)
-        }
-    }
+	const handleConfirm = async () => {
+		if (metaState.chain?.id !== CHAIN_ID) {
+			toast({ status: "error", title: "Wrong ethereum network!", message: "Please switch to Ethereum Mainnet." })
+			return
+		}
+		if (metaState.status[mode] !== "SALE") {
+			toast({ status: "error", title: "NFT is not on sale!", message: "Please comeback later." })
+			return
+		}
+		const balance = await getMetamaskBalance(metaState.accountLogin)
+		if (balance < calculateSlotPrice()) {
+			toast({ status: "error", title: "Insufficient balance!" })
+			return
+		}
+		let checkSC = await checkSmartContract(metaState.accountLogin)
+		if (!checkSC) {
+			toast({ status: "error", title: "Failed to check smart contract!" })
+			return
+		}
+		try {
+			setIsLoadingBtn(true)
+			await mint()
+			setIsLoadingBtn(false)
+			queryClient.invalidateQueries("total-supply")
+			queryClient.invalidateQueries("user-record")
+		} catch (error) {
+			toast({ status: "error", title: "Something went wrong!", message: "Try again later." })
+			setIsLoadingBtn(false)
+		}
+	}
 
-    const getTitle = () =>
-        (metaState.status[mode] === "NOT_FOR_SALE"
-            ? `Waiting for ${mode} sale`
-            : metaState.status[mode] === "SALE"
-            ? `Sipher NFT ${mode} sale`
-            : metaState.status[mode] === "END_SALE"
-            ? `${mode} sale has ended`
-            : "No Sale Available"
-        ).toUpperCase()
+	const getTitle = () =>
+		(metaState.status[mode] === "NOT_FOR_SALE"
+			? `Waiting for ${mode} sale`
+			: metaState.status[mode] === "SALE"
+			? `Sipher NFT ${mode} sale`
+			: metaState.status[mode] === "END_SALE"
+			? `${mode} sale has ended`
+			: "No Sale Available"
+		).toUpperCase()
 
-    const getMaxSlot = () => {
-        if (mode === "public") {
-            return PUBLIC_CAP - (userRecord ? userRecord.publicBought : 0)
-        } else {
-            return userRecord ? Math.max(metaState.isWhitelisted.cap - userRecord.whitelistBought, 0) : 0
-        }
-    }
-    mode === "public"
-        ? PUBLIC_CAP - (userRecord ? userRecord.publicBought : 0)
-        : userRecord
-        ? Math.max(metaState.isWhitelisted.cap - userRecord.whitelistBought, 0)
-        : 0
-    return (
-        <Flex flex={1} flexDir="column" ml={4}>
-            <MyHeading textTransform="uppercase" textAlign="left" color="main.yellow">
-                {getTitle()}
-            </MyHeading>
-            <MyText color="main.brightRed">
-                {metaState.status[mode] === "END_SALE"
-                    ? "Comeback later!"
-                    : metaState.status[mode] === "SALE"
-                    ? "Are you feeling lucky today?"
-                    : "Patience leads to success!"}
-            </MyText>
-            {mode === "public" && metaState.status.public !== "END_SALE" && (
-                <Flex p="2" pt="8">
-                    <ProgressBar
-                        startPrice={START_PRICE}
-                        basePrice={BASE_PRICE}
-                        priceStep={PRICE_STEP}
-                        interval={INTERVAL}
-                        publicSaleTime={saleTime.public}
-                        onPriceChange={handlePriceChange}
-                    />
-                </Flex>
-            )}
-            <Flex justify="space-between" mt="2">
-                <MyText>Choose quantity</MyText>
-                <MyText>{`${userRecord && userRecord[mode === "public" ? "publicBought" : "whitelistBought"]} / ${
-                    mode === "public" ? PUBLIC_CAP : metaState.isWhitelisted.cap
-                }`}</MyText>
-            </Flex>
-            <Counter
-                value={slot}
-                maxValue={getMaxSlot()}
-                onChange={setSlot}
-                isDisabled={metaState.status[mode] !== "SALE"}
-            />
-            <Flex
-                justify="space-between"
-                borderTop="1px"
-                alignItems="center"
-                borderColor="whiteAlpha.300"
-                mt="4"
-                w="100%"
-                pt="2"
-            >
-                <MyText>{`Unit price: ${parseFloat(currentPrice.toFixed(2))} ETH`}</MyText>
-                <MyText ml="auto">
-                    You have purchased: {userRecord ? userRecord.publicBought + userRecord.whitelistBought : "..."}
-                </MyText>
-            </Flex>
-            <Flex pos="relative" mt="4" w="100%" align="center" justify="space-between">
-                <MyText size="large">
-                    You will pay:
-                    <chakra.span mx="2" color="main.yellow" fontWeight="bold">
-                        {metaState.status[mode] !== "END_SALE" ? calculateSlotPrice() : 0}
-                    </chakra.span>
-                    ETH
-                </MyText>
-                <Button
-                    ml="auto"
-                    bg="red.500"
-                    fontSize="sm"
-                    isLoading={isLoadingBtn}
-                    w="12rem"
-                    loadingText="MINTING"
-                    _hover={{ bg: "red.400" }}
-                    _active={{ bg: "red.600" }}
-                    _focus={{ shadow: "none" }}
-                    onClick={handleConfirm}
-                    isDisabled={
-                        isLoadingRecord ||
-                        slot === 0 ||
-                        metaState.status[mode] === "NOT_FOR_SALE" ||
-                        metaState.status[mode] === "END_SALE"
-                    }
-                >
-                    {metaState.status[mode] === "NOT_FOR_SALE"
-                        ? "STARTING SOON"
-                        : metaState.status[mode] === "END_SALE"
-                        ? "NOT YET AVAILABLE"
-                        : "MINT NOW"}
-                </Button>
-            </Flex>
-        </Flex>
-    )
+	const getMaxSlot = () => {
+		if (mode === "public") {
+			return PUBLIC_CAP - (userRecord ? userRecord.publicBought : 0)
+		} else {
+			return userRecord ? Math.max(metaState.isWhitelisted.cap - userRecord.whitelistBought, 0) : 0
+		}
+	}
+	mode === "public"
+		? PUBLIC_CAP - (userRecord ? userRecord.publicBought : 0)
+		: userRecord
+		? Math.max(metaState.isWhitelisted.cap - userRecord.whitelistBought, 0)
+		: 0
+	return (
+		<Flex flex={1} flexDir="column" ml={4}>
+			<MyHeading textTransform="uppercase" textAlign="left" color="main.yellow">
+				{getTitle()}
+			</MyHeading>
+			<MyText color="main.brightRed">
+				{metaState.status[mode] === "END_SALE"
+					? "Comeback later!"
+					: metaState.status[mode] === "SALE"
+					? "Are you feeling lucky today?"
+					: "Patience leads to success!"}
+			</MyText>
+			{mode === "public" && metaState.status.public !== "END_SALE" && (
+				<Flex p="2" pt="8">
+					<ProgressBar
+						status={metaState.status.public}
+						startPrice={START_PRICE}
+						basePrice={BASE_PRICE}
+						priceStep={PRICE_STEP}
+						interval={INTERVAL}
+						publicSaleTime={saleTime.public}
+						onPriceChange={handlePriceChange}
+					/>
+				</Flex>
+			)}
+			<Flex justify="space-between" mt="2">
+				<MyText>Choose quantity</MyText>
+				<MyText>{`${userRecord && userRecord[mode === "public" ? "publicBought" : "whitelistBought"]} / ${
+					mode === "public" ? PUBLIC_CAP : metaState.isWhitelisted.cap
+				}`}</MyText>
+			</Flex>
+			<Counter
+				value={slot}
+				maxValue={getMaxSlot()}
+				onChange={setSlot}
+				isDisabled={metaState.status[mode] !== "SALE"}
+			/>
+			<Flex
+				justify="space-between"
+				borderTop="1px"
+				alignItems="center"
+				borderColor="whiteAlpha.300"
+				mt="4"
+				w="100%"
+				pt="2"
+			>
+				<MyText>{`Unit price: ${parseFloat(currentPrice.toFixed(2))} ETH`}</MyText>
+				<MyText ml="auto">
+					You have purchased: {userRecord ? userRecord.publicBought + userRecord.whitelistBought : "..."}
+				</MyText>
+			</Flex>
+			<Flex pos="relative" mt="4" w="100%" align="center" justify="space-between">
+				<MyText size="large">
+					You will pay:
+					<chakra.span mx="2" color="main.yellow" fontWeight="bold">
+						{metaState.status[mode] !== "END_SALE" ? calculateSlotPrice() : 0}
+					</chakra.span>
+					ETH
+				</MyText>
+				<Button
+					ml="auto"
+					bg="red.500"
+					fontSize="sm"
+					isLoading={isLoadingBtn}
+					w="12rem"
+					loadingText="MINTING"
+					_hover={{ bg: "red.400" }}
+					_active={{ bg: "red.600" }}
+					_focus={{ shadow: "none" }}
+					onClick={handleConfirm}
+					isDisabled={
+						isLoadingRecord ||
+						slot === 0 ||
+						metaState.status[mode] === "NOT_FOR_SALE" ||
+						metaState.status[mode] === "END_SALE"
+					}
+				>
+					{metaState.status[mode] === "NOT_FOR_SALE"
+						? "STARTING SOON"
+						: metaState.status[mode] === "END_SALE"
+						? "NOT YET AVAILABLE"
+						: "MINT NOW"}
+				</Button>
+			</Flex>
+		</Flex>
+	)
 }
 
 export default SaleForm
--- web-minting/src/constant/index.ts
+++ web-minting/src/constant/index.ts
@@ -1,7 +1,7 @@
 // Sale contract address
-export const SMARTCONTRACT_SALE_NEKO = "0xC0b2F8efb94DC5A08dc5D0b9aA30EC260C101428"
+export const SMARTCONTRACT_SALE_NEKO = "0x85841676f8F97e738997D5F846aBF86CD3116b40"
 // Neko contract address
-export const SMARTCONTRACT_NEKO = "0xe769d459A28e1a5693a79B2644DfD06aea066772"
+export const SMARTCONTRACT_NEKO = "0x452b912F86780b26a8d830036541C77c08C74276"
 // Rinkeby chain id
 export const CHAIN_ID = "4"
 // Starting price of public sale
@@ -9,8 +9,8 @@ export const START_PRICE = 1
 // Base price of public sale
 export const BASE_PRICE = 0.1
 // Price step
-export const PRICE_STEP = 0.1
+export const PRICE_STEP = 0.08
 // Interval: 10 MINUTES
-export const INTERVAL = 180 * 1000
+export const INTERVAL = 300 * 1000
 // Public slot cap
 export const PUBLIC_CAP = 5
