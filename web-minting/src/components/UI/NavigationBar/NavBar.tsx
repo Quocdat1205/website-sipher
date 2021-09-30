02da84179e6d5abec9e6b202a50db80f0a886741
sieutihons10 <sieutihons10@gmail.com>
2021-09-30 14:30:17 +0700

    change logo png to logo svg

--- web-minting/src/components/UI/NavigationBar/NavBar.tsx
+++ web-minting/src/components/UI/NavigationBar/NavBar.tsx
@@ -7,21 +7,21 @@ import useWalletContext from "@hooks/useWalletContext"
 interface NavBarProps {}
 
 export const navMenus = [
-    { id: "Private Sale", path: "/private-minting" },
-    { id: "Public Sale", path: "/public-minting" },
-    { id: "Inventory", path: "/inventory" },
+	{ id: "Private Sale", path: "/private-minting" },
+	{ id: "Public Sale", path: "/public-minting" },
+	{ id: "Inventory", path: "/inventory" },
 ]
 
 export const NavBar = ({}: NavBarProps) => {
-    const { metaState } = useWalletContext()
+	const { metaState } = useWalletContext()
 
-    return (
-        <BaseNavigationBar
-            logoPath="/images/mainlogo.png"
-            menus={navMenus.filter(item => item.id !== "Private Sale" || metaState.isWhitelisted.proof.length > 0)}
-        >
-            <TotalSupplyNFTs />
-            <AccountAddress />
-        </BaseNavigationBar>
-    )
+	return (
+		<BaseNavigationBar
+			logoPath="/images/mainlogo.svg"
+			menus={navMenus.filter((item) => item.id !== "Private Sale" || metaState.isWhitelisted.proof.length > 0)}
+		>
+			<TotalSupplyNFTs />
+			<AccountAddress />
+		</BaseNavigationBar>
+	)
 }
