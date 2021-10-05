173a1987a280099bf627ccfd75d59c36a6974802
sieutihons10 <sieutihons10@gmail.com>
2021-10-05 14:02:33 +0700

    update new getSaleConfig

--- web-minting/src/api/smartContract.ts
+++ web-minting/src/api/smartContract.ts
@@ -4,29 +4,33 @@ import { SMARTCONTRACT_SALE_NEKO, SMARTCONTRACT_NEKO } from "@constant/index"
 
 /** Check smart contract! I don't know about this */
 export const checkSmartContract = async (publicAddress: string): Promise<boolean> => {
-    let address = publicAddress.toLowerCase()
-    const { data } = await axios.get(
-        `/neko-sc/checkSC?nftContractAddress=${SMARTCONTRACT_NEKO}&saleContractAddress=${SMARTCONTRACT_SALE_NEKO}&WalletAddress=${address}`,
-        config
-    )
+	let address = publicAddress.toLowerCase()
+	const { data } = await axios.get(
+		`/neko-sc/checkSC?nftContractAddress=${SMARTCONTRACT_NEKO}&saleContractAddress=${SMARTCONTRACT_SALE_NEKO}&WalletAddress=${address}`,
+		config
+	)
 
-    return data.message
+	return data.message
 }
 
 /** Check gas! I don't know for sure */
 export const checkGas = async () => {
-    const { data } = await axios.get(`/neko-sc/checkgas`, config)
-    return data.message
+	const { data } = await axios.get(`/neko-sc/checkgas`, config)
+	return data.message
 }
 
 /** Check if an address is whitelisted
  * @returns proof: string[]
- * @returns cap: number
+ * @returns privateCap: number
+ * @returns freeCap: number
  */
-export const checkIsWhitelisted = async (publicAddress: string): Promise<{ proof: string[]; cap: number }> => {
-    const { data } = await axios.get(
-        `/neko-sc/checkWhiteList?saleContractAddress=${SMARTCONTRACT_SALE_NEKO}&WalletAddress=${publicAddress}`,
-        config
-    )
-    return { proof: data.message.proof, cap: data.message.cap }
+export const checkIsWhitelisted = async (
+	publicAddress: string
+): Promise<{ proof: string[]; privateCap: number; freeCap: number }> => {
+	const { data } = await axios.get(
+		`/neko-sc/checkWhiteList?saleContractAddress=${SMARTCONTRACT_SALE_NEKO}&WalletAddress=${publicAddress}`,
+		config
+	)
+	//new change
+	return { proof: data.message.proof, privateCap: data.message.privateCap, freeCap: data.message.freeCap }
 }
--- web-minting/src/components/Provider.tsx
+++ web-minting/src/components/Provider.tsx
@@ -7,26 +7,26 @@ import store from "../store"
 import { WalletProvider } from "@hooks/useWalletContext"
 
 interface ProviderProps {
-    children: React.ReactNode
+	children: React.ReactNode
 }
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-            retry: 1,
-        },
-    },
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			retry: 1,
+		},
+	},
 })
 const Provider: FC<ProviderProps> = ({ children }) => {
-    return (
-        <ChakraProvider theme={theme}>
-            <QueryClientProvider client={queryClient}>
-                <StoreProvider store={store}>
-                    <WalletProvider>{children}</WalletProvider>
-                </StoreProvider>
-            </QueryClientProvider>
-        </ChakraProvider>
-    )
+	return (
+		<ChakraProvider theme={theme}>
+			<QueryClientProvider client={queryClient}>
+				<StoreProvider store={store}>
+					<WalletProvider>{children}</WalletProvider>
+				</StoreProvider>
+			</QueryClientProvider>
+		</ChakraProvider>
+	)
 }
 
 export default Provider
