6041d7be288ae67c6566f44a7489b74a68d84dc7
vulh1209 <vu.le@sipher.xyz>
2021-10-14 21:12:45 +0700

    update unity anim 3

--- web-landing/src/utils/unity.ts
+++ web-landing/src/utils/unity.ts
@@ -1,10 +1,10 @@
 import { UnityContext } from "react-unity-webgl"
 
 export const unityContext = new UnityContext({
-    loaderUrl: "/unity/unity6.loader.js",
-    dataUrl: "/unity/unity6.data",
-    frameworkUrl: "/unity/unity6.framework.js",
-    codeUrl: "/unity/unity6.wasm",
+    loaderUrl: "/unity/unity5.loader.js",
+    dataUrl: "/unity/unity5.data",
+    frameworkUrl: "/unity/unity5.framework.js",
+    codeUrl: "/unity/unity5.wasm",
     webglContextAttributes: {
         powerPreference: "low-power",
         preserveDrawingBuffer: true,
