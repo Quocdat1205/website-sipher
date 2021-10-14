6b9e72bd81aa5e99735624b813c27b8980e3ecd7
vulh1209 <vu.le@sipher.xyz>
2021-10-14 17:22:00 +0700

    update unity metal effect

--- web-landing/src/utils/unity.ts
+++ web-landing/src/utils/unity.ts
@@ -1,10 +1,10 @@
 import { UnityContext } from "react-unity-webgl"
 
 export const unityContext = new UnityContext({
-    loaderUrl: "/unity/unity1.loader.js",
-    dataUrl: "/unity/unity1.data",
-    frameworkUrl: "/unity/unity1.framework.js",
-    codeUrl: "/unity/unity1.wasm",
+    loaderUrl: "/unity/unity2.loader.js",
+    dataUrl: "/unity/unity2.data",
+    frameworkUrl: "/unity/unity2.framework.js",
+    codeUrl: "/unity/unity2.wasm",
     webglContextAttributes: {
         powerPreference: "low-power",
         preserveDrawingBuffer: true,
