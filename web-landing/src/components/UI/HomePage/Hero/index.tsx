1607ad542346f8c6adc498f7a7edc766606eac97
vulh1209 <vu.le@sipher.xyz>
2021-10-14 09:41:14 +0700

    update unity new effect & mobile

--- web-landing/src/components/UI/HomePage/Hero/index.tsx
+++ web-landing/src/components/UI/HomePage/Hero/index.tsx
@@ -1,99 +1,105 @@
-import { Box, Flex } from "@chakra-ui/react"
-import unityContext from "src/utils/unity"
-import { useStoreActions } from "@store"
-import FirstScreen from "./FirstScreen"
-import HeroScreen from "./HeroScreen"
-import AmountScreen from "./AmountScreen"
-import CountDown from "./CountDown"
-import { Typo } from "@components/shared"
-import Unity from "react-unity-webgl"
-import { MouseEvent, MouseEventHandler, useEffect, useState, useRef } from "react"
+import { Box, Flex } from "@chakra-ui/react";
+import unityContext from "src/utils/unity";
+import { useStoreActions } from "@store";
+import FirstScreen from "./FirstScreen";
+import HeroScreen from "./HeroScreen";
+import AmountScreen from "./AmountScreen";
+import CountDown from "./CountDown";
+import { Typo } from "@components/shared";
+import Unity from "react-unity-webgl";
+import { MouseEvent, MouseEventHandler, useEffect, useState, useRef } from "react";
 interface HeroProps {}
 
 const content =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore voluptates inventore soluta rerum similique totam facilis cupiditate, cum aut incidunt quod. Consequatur eveniet, laborum, cumque itaque officiis rem inventore iste sequi dicta dolorum magnam reiciendis aut nemo, hic nam doloremque eius nostrum quisquam accusantium ducimus aliquid earum? Dignissimos, placeat velit?"
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore voluptates inventore soluta rerum similique totam facilis cupiditate, cum aut incidunt quod. Consequatur eveniet, laborum, cumque itaque officiis rem inventore iste sequi dicta dolorum magnam reiciendis aut nemo, hic nam doloremque eius nostrum quisquam accusantium ducimus aliquid earum? Dignissimos, placeat velit?";
 
 export const variants = {
-    visible: { opacity: 1, y: 0, transformOrigin: "50% 100% 0" },
-    hidden: { opacity: 0, y: 100, transformOrigin: "50% 100% 0" },
-}
+  visible: { opacity: 1, y: 0, transformOrigin: "50% 100% 0" },
+  hidden: { opacity: 0, y: 100, transformOrigin: "50% 100% 0" },
+};
 
 const Hero = ({}: HeroProps) => {
-    const setInitialLoading = useStoreActions(action => action.setInitialLoading)
-    unityContext.on("loaded", () =>
-        setTimeout(() => {
-            setInitialLoading(false)
-        }, 2000)
-    )
-    // const [scrollY, setScrollY] = useState(0)
-    const [delay, setDelay] = useState(false)
-    useEffect(() => {
-        let timeout: NodeJS.Timeout
-        if (delay) {
-            timeout = setTimeout(() => {
-                setDelay(false)
-            }, 50)
-        }
-        return () => clearTimeout(timeout)
-    }, [delay, setDelay])
-
-    const ctnRef = useRef<HTMLDivElement>(null)
-    const handleMouseMWheel = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
-        if (ctnRef.current)
-            unityContext.send("Main Camera", "angle", (window.scrollY / ctnRef.current.clientHeight) * 5)
+  const setInitialLoading = useStoreActions((action) => action.setInitialLoading);
+  unityContext.on("loaded", () =>
+    setTimeout(() => {
+      setInitialLoading(false);
+    }, 2000)
+  );
+  const [scrollY, setScrollY] = useState(0);
+  const [delay, setDelay] = useState(false);
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    if (delay) {
+      timeout = setTimeout(() => {
+        setDelay(false);
+      }, 50);
     }
-    const handleMouseMove = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
-        if (delay) return
-        else {
-            unityContext.send("Main Camera", "effectNekoX", e.clientX / window.innerWidth)
-            unityContext.send("Main Camera", "effectNekoY", 1 - e.clientY / window.innerHeight)
-            // setDelay(true)
-        }
+    return () => clearTimeout(timeout);
+  }, [delay, setDelay]);
+
+  const ctnRef = useRef<HTMLDivElement>(null);
+  // const handleMouseMWheel = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+  //     console.log("1");
+  //     if (ctnRef.current)
+  //         unityContext.send("Main Camera", "angle", (window.scrollY / ctnRef.current.clientHeight) * 5)
+  // }
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScrollY(window.scrollY);
+      unityContext.send("Main Camera", "angle", (window.scrollY / ctnRef.current.clientHeight) * 5)
+    });
+    return () =>
+      window.removeEventListener("scroll", () => {
+        setScrollY(window.scrollY);
+      });
+  }, [scrollY]);
+  const handleMouseMove = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+    if (delay) return;
+    else {
+      unityContext.send("Main Camera", "effectNekoX", e.clientX / window.innerWidth);
+      unityContext.send("Main Camera", "effectNekoY", 1 - e.clientY / window.innerHeight);
+      // setDelay(true)
     }
+  };
 
-    return (
-        <Box
-            pt={[24, 0, 0]}
-            pos="relative"
-            zIndex={0}
-            overflowX="hidden"
-            onMouseMove={handleMouseMove}
-            onWheel={handleMouseMWheel}
-            ref={ctnRef}
-        >
-            <Flex direction="column" w="full">
-                <FirstScreen />
-                <HeroScreen
-                    position="L"
-                    heading={<CountDown deadline={1635645600000} />}
-                    content={content}
-                    heading2=""
-                />
-                <AmountScreen />
-                <HeroScreen
-                    position="L"
-                    heading={
-                        <Typo.Heading isGradient textAlign="left" fontWeight={900} fontSize={["4rem", "5rem"]} mb={0}>
-                            Benefits
-                        </Typo.Heading>
-                    }
-                    content={content}
-                />
-                <HeroScreen
-                    position="R"
-                    heading={
-                        <Typo.Heading isGradient textAlign="left" fontWeight={900} fontSize={["4rem", "5rem"]} mb={0}>
-                            Play to earn
-                        </Typo.Heading>
-                    }
-                    content={content}
-                />
-            </Flex>
-            <Box pos="fixed" top={0} left={0} h="full" w="full">
-                <Unity unityContext={unityContext} style={{ width: "100%", height: "100%" }} />
-            </Box>
-        </Box>
-    )
-}
+  return (
+    <Box
+      pt={[24, 0, 0]}
+      pos="relative"
+      zIndex={0}
+      overflowX="hidden"
+      onMouseMove={handleMouseMove}
+      // onScroll={handleMouseMWheel}
+      ref={ctnRef}
+    >
+      <Flex direction="column" w="full">
+        <FirstScreen />
+        <HeroScreen position="L" heading={<CountDown deadline={1635645600000} />} content={content} heading2="" />
+        <AmountScreen />
+        <HeroScreen
+          position="L"
+          heading={
+            <Typo.Heading isGradient textAlign="left" fontWeight={900} fontSize={["4rem", "5rem"]} mb={0}>
+              Benefits
+            </Typo.Heading>
+          }
+          content={content}
+        />
+        <HeroScreen
+          position="R"
+          heading={
+            <Typo.Heading isGradient textAlign="left" fontWeight={900} fontSize={["4rem", "5rem"]} mb={0}>
+              Play to earn
+            </Typo.Heading>
+          }
+          content={content}
+        />
+      </Flex>
+      <Box pos="fixed" top={0} left={0} h="full" w="full">
+        <Unity unityContext={unityContext} style={{ width: "100%", height: "100%" }} />
+      </Box>
+    </Box>
+  );
+};
 
-export default Hero
+export default Hero;
