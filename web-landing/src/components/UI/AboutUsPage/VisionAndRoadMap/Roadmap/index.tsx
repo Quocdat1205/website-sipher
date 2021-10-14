b6bfbbc66158eb85b4fcab4d5284460e9af2e2ba
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-10-14 22:50:24 +0700

    Update index.tsx

--- web-landing/src/components/UI/AboutUsPage/VisionAndRoadMap/Roadmap/index.tsx
+++ web-landing/src/components/UI/AboutUsPage/VisionAndRoadMap/Roadmap/index.tsx
@@ -1,18 +1,18 @@
-import { Accordion, chakra } from "@chakra-ui/react"
-import { TextContainer } from "@components/shared"
-import React from "react"
-import MyAccordionItem from "./MyAccordionItem"
-import content from "@constant/content/roadmap"
+import { Accordion, chakra } from "@chakra-ui/react";
+import { TextContainer } from "@components/shared";
+import React from "react";
+import MyAccordionItem from "./MyAccordionItem";
+import content from "@constant/content/roadmap";
 
 const RoadMap = () => {
-    return (
-        <TextContainer headline="ROADMAP">
-            <Accordion defaultIndex={[0]} allowMultiple>
-                {content.map(item => (
-                    <MyAccordionItem key={item.name} month={item.month} name={item.name} phases={item.phases} />
-                ))}
-            </Accordion>
-        </TextContainer>
-    )
-}
-export default RoadMap
+  return (
+    <TextContainer headline="ROADMAP">
+      <Accordion defaultIndex={[2]} allowMultiple>
+        {content.map((item) => (
+          <MyAccordionItem key={item.name} month={item.month} name={item.name} phases={item.phases} />
+        ))}
+      </Accordion>
+    </TextContainer>
+  );
+};
+export default RoadMap;
