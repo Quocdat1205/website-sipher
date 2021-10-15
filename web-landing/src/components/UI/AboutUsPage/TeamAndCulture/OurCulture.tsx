41821c6e314ebbdf9ed75670f2215a363d4c8954
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-10-15 21:49:00 +0700

    update content faq

--- web-landing/src/components/UI/AboutUsPage/TeamAndCulture/OurCulture.tsx
+++ web-landing/src/components/UI/AboutUsPage/TeamAndCulture/OurCulture.tsx
@@ -4,9 +4,7 @@ import React from "react";
 const OurCulture = () => {
   return (
     <TextContainer headline="OUR CULTURE" mb={24}>
-      <Typo.Text textAlign="center">
-        We are a super small team. We are young and full of energy. We love gaming.
-      </Typo.Text>
+      <Typo.Text textAlign="center">We are a growing team. We are young and full of energy. We love gaming.</Typo.Text>
       <Typo.Text textAlign="center">We love amazing stories told through gaming and movies.</Typo.Text>
       <Typo.Text textAlign="center">We aim to create a culture where we grow as a team,</Typo.Text>
       <Typo.Text textAlign="center">let the best ideas win, and work hard to achieve our goals.</Typo.Text>
