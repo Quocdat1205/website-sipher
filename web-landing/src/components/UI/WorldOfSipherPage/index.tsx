39bc32012318d515672402ff9dcc92ea310e0a22
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-10-15 15:23:51 +0700

    update IMG

--- web-landing/src/components/UI/WorldOfSipherPage/index.tsx
+++ web-landing/src/components/UI/WorldOfSipherPage/index.tsx
@@ -1,80 +1,75 @@
-import { Flex } from "@chakra-ui/react"
-import { blockchainContent, factionsContent, gameplayContent, theWorldContent } from "@constant/content/why"
-import React, { useState } from "react"
-import MainContent from "./MainContent"
-import TheWorldContent from "./TheWorldContent"
-import GameplayContent from "./GameplayContent"
-import FactionsContent from "./FactionsContent"
-import BlockchainContent from "./BlockchainContent"
-import { LinkButton, BackgroundContainer, HeaderBackground } from "@components/shared"
+import { Flex } from "@chakra-ui/react";
+import { blockchainContent, factionsContent, gameplayContent, theWorldContent } from "@constant/content/why";
+import React, { useState } from "react";
+import MainContent from "./MainContent";
+import TheWorldContent from "./TheWorldContent";
+import GameplayContent from "./GameplayContent";
+import FactionsContent from "./FactionsContent";
+import BlockchainContent from "./BlockchainContent";
+import { LinkButton, BackgroundContainer, HeaderBackground } from "@components/shared";
 
-type Page = "theworld" | "gameplay" | "factions" | "blockchain"
+type Page = "theworld" | "gameplay" | "factions" | "blockchain";
 
 const WorldOfSipherUI = () => {
-    const [currentPage, setCurrentPage] = useState<Page>("theworld")
+  const [currentPage, setCurrentPage] = useState<Page>("theworld");
 
-    const renderImage = () =>
-        `/images/pc/${
-            currentPage === "theworld"
-                ? "why/why1.png"
-                : currentPage === "gameplay"
-                ? "why/gameplay1.png"
-                : currentPage === "factions"
-                ? "nft/banner.png"
-                : "news.png"
-        }`
+  const renderImage = () =>
+    `/images/pc/${
+      currentPage === "theworld"
+        ? "why/theworld.png"
+        : currentPage === "gameplay"
+        ? "why/gameplay.png"
+        : currentPage === "factions"
+        ? "nft/banner.png"
+        : "news.png"
+    }`;
 
-    const renderHeadline = () =>
-        currentPage === "theworld"
-            ? "The world"
-            : currentPage === "gameplay"
-            ? "Gameplay"
-            : currentPage === "factions"
-            ? "Factions"
-            : "Blockchain"
+  const renderHeadline = () =>
+    currentPage === "theworld"
+      ? "The world"
+      : currentPage === "gameplay"
+      ? "Gameplay"
+      : currentPage === "factions"
+      ? "Factions"
+      : "Blockchain";
 
-    const renderContent = () =>
-        currentPage === "theworld"
-            ? theWorldContent.worldBlockCategory
-            : currentPage === "gameplay"
-            ? gameplayContent.gameCategory
-            : currentPage === "factions"
-            ? factionsContent.main
-            : blockchainContent.main
+  const renderContent = () =>
+    currentPage === "theworld"
+      ? theWorldContent.worldBlockCategory
+      : currentPage === "gameplay"
+      ? gameplayContent.gameCategory
+      : currentPage === "factions"
+      ? factionsContent.main
+      : blockchainContent.main;
 
-    return (
-        <BackgroundContainer px={0}>
-            <HeaderBackground
-                title="WORLD OF SIPHER"
-                description="Learn why we're the next awesome blockchain game you'll play!"
-            />
-            <Flex flex={1} direction="column" w="full" align="center" py={24} px={4}>
-                <MainContent
-                    srcImg={renderImage()}
-                    headline={renderHeadline()}
-                    content={renderContent()}
-                    currentPage={currentPage}
-                    setCurrentPage={setCurrentPage}
-                >
-                    {currentPage === "theworld" ? (
-                        <TheWorldContent />
-                    ) : currentPage === "gameplay" ? (
-                        <GameplayContent />
-                    ) : currentPage === "factions" ? (
-                        <FactionsContent />
-                    ) : (
-                        <BlockchainContent />
-                    )}
-                </MainContent>
-                <LinkButton
-                    text="View The Sipher Atlas"
-                    href="https://discord.com/invite/dRqdSxUSmd"
-                    size="large"
-                    mt={8}
-                />
-            </Flex>
-        </BackgroundContainer>
-    )
-}
+  return (
+    <BackgroundContainer px={0}>
+      <HeaderBackground
+        title="WORLD OF SIPHER"
+        description="Learn why we're the next awesome blockchain game you'll play!"
+      />
+      <Flex flex={1} direction="column" w="full" align="center" py={24} px={4}>
+        <MainContent
+          srcImg={renderImage()}
+          headline={renderHeadline()}
+          content={renderContent()}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        >
+          {currentPage === "theworld" ? (
+            <TheWorldContent />
+          ) : currentPage === "gameplay" ? (
+            <GameplayContent />
+          ) : currentPage === "factions" ? (
+            <FactionsContent />
+          ) : (
+            <BlockchainContent />
+          )}
+        </MainContent>
+        <LinkButton text="View The Sipher Atlas" href="https://discord.com/invite/dRqdSxUSmd" size="large" mt={8} />
+      </Flex>
+    </BackgroundContainer>
+  );
+};
 
-export default WorldOfSipherUI
+export default WorldOfSipherUI;
