a1484feac63e101f1cace68bd85582a7aa2aadf7
vulh1209 <vu.le@sipher.xyz>
2021-10-15 15:02:42 +0700

    pull

--- web-landing/src/utils/unity.ts
+++ web-landing/src/utils/unity.ts
@@ -1,9 +1,8 @@
-// import { isMobile } from "react-device-detect";
+import { isMobile } from "react-device-detect";
 import { UnityContext } from "react-unity-webgl";
 
-// const isMobileOrApple = isMobile ? true : false;
+const isMobileOrApple = isMobile ? true : false;
 
-<<<<<<< HEAD
 export const unityContext = isMobileOrApple
   ? new UnityContext({
       loaderUrl: "/unity/mobile.loader.js",
@@ -16,38 +15,14 @@ export const unityContext = isMobileOrApple
       },
     })
   : new UnityContext({
-      loaderUrl: "/unity/unity11.loader.js",
-      dataUrl: "/unity/unity11.data",
-      frameworkUrl: "/unity/unity11.framework.js",
-      codeUrl: "/unity/unity11.wasm",
+      loaderUrl: "/unity/unity9.loader.js",
+      dataUrl: "/unity/unity9.data",
+      frameworkUrl: "/unity/unity9.framework.js",
+      codeUrl: "/unity/unity9.wasm",
       webglContextAttributes: {
         powerPreference: "low-power",
         preserveDrawingBuffer: true,
       },
     });
-=======
-export const unityContext =
-  // isMobileOrApple
-  //   ? new UnityContext({
-  //       loaderUrl: "/unity/mobile.loader.js",
-  //       dataUrl: "/unity/mobile.data",
-  //       frameworkUrl: "/unity/mobile.framework.js",
-  //       codeUrl: "/unity/mobile.wasm",
-  //       webglContextAttributes: {
-  //         powerPreference: "low-power",
-  //         preserveDrawingBuffer: true,
-  //       },
-  //     })
-  new UnityContext({
-    loaderUrl: "/unity/unity9.loader.js",
-    dataUrl: "/unity/unity9.data",
-    frameworkUrl: "/unity/unity9.framework.js",
-    codeUrl: "/unity/unity9.wasm",
-    webglContextAttributes: {
-      powerPreference: "low-power",
-      preserveDrawingBuffer: true,
-    },
-  });
->>>>>>> f02f463f74cd6db9b1c653864e2cf768f9fe3d98
 
 export default unityContext;
