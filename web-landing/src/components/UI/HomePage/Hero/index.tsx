200cec61ae4807fe79c86ca481551f9b3e5523a4
La Quoc Anh <anh.lq@trungthuy.vn>
2021-10-15 19:33:51 +0700

    fix(web-landing): finishing

--- web-landing/src/components/UI/HomePage/Hero/index.tsx
+++ web-landing/src/components/UI/HomePage/Hero/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Img, Grid } from "@chakra-ui/react"
+import { Box, Flex, Img } from "@chakra-ui/react"
 import unityContext from "src/utils/unity"
 import { useStoreActions } from "@store"
 import FirstScreen from "./FirstScreen"
--- web-landing/src/hooks/api/config.ts
+++ web-landing/src/hooks/api/config.ts
@@ -1,6 +1,6 @@
-export const baseURL = "https://be.sipherion.com/api/sipher/v1.1";
+export const baseURL = "https://be.sipher.xyz/api/sipher/v1.1"
 // export const baseUrl = "http://localhost:5000/api/sipher/v1.0"
 
-const config = { baseURL };
+const config = { baseURL }
 
-export default config;
+export default config
