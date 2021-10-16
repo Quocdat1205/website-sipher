b0fc012d287a7a57eef82876076a1f6eb3360fcc
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-10-16 12:07:03 +0700

    update fix

--- web-landing/src/components/UI/NewsPage/LayoutMedium.tsx
+++ web-landing/src/components/UI/NewsPage/LayoutMedium.tsx
@@ -1,40 +1,46 @@
-import React from "react"
-import ReactHtmlParser from "react-html-parser"
-import { Box, Flex, HStack } from "@chakra-ui/layout"
-import { DetailsNewsProps } from "./PopupCard"
-import UrlCopier from "./UrlCopier"
-import ButtonLinkTo from "./ButtonLinkTo"
-import { MyHeading } from "@sipher/web-components"
+import React from "react";
+import ReactHtmlParser from "react-html-parser";
+import { Box, Flex, HStack } from "@chakra-ui/layout";
+import { DetailsNewsProps } from "./PopupCard";
+import UrlCopier from "./UrlCopier";
+import ButtonLinkTo from "./ButtonLinkTo";
+import { MyHeading } from "@sipher/web-components";
 
 interface Props {
-	details: DetailsNewsProps
-	navbarHeight: number
+  details: DetailsNewsProps;
 }
 
-const LayoutMedium = ({ details, navbarHeight }: Props) => {
-	const height = `calc(100vh - ${navbarHeight}px)`
+const LayoutMedium = ({ details }: Props) => {
+  return (
+    <Flex pt={["64px", "66px"]} overflow="auto" flexDir="column" h="100vh">
+      <Box maxW="64rem">
+        <Flex flexDir="column" flex={1} p={[4, 8]}>
+          <Box flex={1}>
+            <MyHeading textAlign="center" size="large">
+              {details.title}
+            </MyHeading>
+            <Box
+              mt={[4, 6]}
+              sx={{ ul: { listStylePos: "inside" }, img: { m: "0 auto", py: 8, maxHeight: "45rem" } }}
+              color="about.textGray"
+            >
+              {ReactHtmlParser(details.content && details.content)}
+            </Box>
+            <Box
+              sx={{ ul: { listStylePos: "inside" }, img: { m: "0 auto", py: 8, maxHeight: "45rem" } }}
+              color="about.textGray"
+            >
+              {ReactHtmlParser(details.description && details.description)}
+            </Box>
+          </Box>
+          <HStack spacing={8} px={8} py={4} borderTop="1px" borderColor="about.textGray">
+            <UrlCopier url={window.location.href} />
+            <ButtonLinkTo url={details.link} typeSocial={details.type} />
+          </HStack>
+        </Flex>
+      </Box>
+    </Flex>
+  );
+};
 
-	return (
-		<Flex overflow="auto" alignItems="center" flexDir="column"  h={height}>
-			<Box maxW="64rem">
-			<Flex flexDir="column" flex={1}  p={[4, 8]}>
-				<Box flex={1} >
-					<MyHeading textAlign="center" size="large">{details.title}</MyHeading>
-					<Box mt={[4, 6]} sx={{ul:{listStylePos: "inside"} ,img: { m: "0 auto", py: 8, maxHeight: "45rem" } }} color="about.textGray">
-						{ReactHtmlParser(details.content && details.content)}
-					</Box>
-					<Box sx={{ul:{listStylePos: "inside"} ,img: { m: "0 auto", py: 8, maxHeight: "45rem" } }} color="about.textGray">
-						{ReactHtmlParser(details.description && details.description)}
-					</Box>
-				</Box>
-				<HStack spacing={8} px={8} py={4} borderTop="1px" borderColor="about.textGray">
-					<UrlCopier url={window.location.href} />
-					<ButtonLinkTo url={details.link} typeSocial={details.type} />
-				</HStack>
-			</Flex>
-			</Box>
-		</Flex>
-	)
-}
-
-export default LayoutMedium
+export default LayoutMedium;
