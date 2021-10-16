e34b167e54309bfb7498901805791f549922a8e7
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-10-16 09:11:26 +0700

    unset cursor icon

--- web-landing/src/components/shared/CommunityIcon/index.tsx
+++ web-landing/src/components/shared/CommunityIcon/index.tsx
@@ -1,34 +1,35 @@
 // * DESCRIPTION:
 
-import { Box, BoxProps } from "@chakra-ui/layout"
-import { useState, Fragment } from "react"
-import SvgGradient from "./SvgGradient"
-import SvgRect from "./SvgRect"
-import IconData from "./IconData"
+import { Box, BoxProps } from "@chakra-ui/layout";
+import SvgGradient from "./SvgGradient";
+import SvgRect from "./SvgRect";
+import IconData from "./IconData";
 
 export interface IconProps extends BoxProps {
-    icon: keyof typeof IconData
+  icon: keyof typeof IconData;
 }
 
 export const CommunityIcon = ({ icon, ...rest }: IconProps) => {
-    const [active, setActive] = useState(false)
-    return (
-        <Box
-            onMouseEnter={() => setActive(true)}
-            onMouseLeave={() => setActive(false)}
-            cursor="pointer"
-            {...rest}
-            h="full"
-        >
-            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
-                <SvgRect active={active} />
-                <SvgGradient />
-                {IconData[icon].map((path, idx) => (
-                    <path key={idx} d={path} fill={active ? "url(#MyGradient)" : "white"} />
-                ))}
-            </svg>
-        </Box>
-    )
-}
+  //hover icon
+  //   const [active, setActive] = useState(false);
+  return (
+    <Box
+      //   onMouseEnter={() => setActive(true)}
+      //   onMouseLeave={() => setActive(false)}
+      // cursor="pointer"
+      {...rest}
+      h="full"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
+        <SvgRect active={false} />
+        <SvgGradient />
+        {IconData[icon].map((path, idx) => (
+          //   <path key={idx} d={path} fill={active ? "url(#MyGradient)" : "white"} />
+          <path key={idx} d={path} fill="white" />
+        ))}
+      </svg>
+    </Box>
+  );
+};
 
-export default CommunityIcon
+export default CommunityIcon;
