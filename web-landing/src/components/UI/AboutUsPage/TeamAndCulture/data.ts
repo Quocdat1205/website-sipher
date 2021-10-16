4ab9ec09532230d25725a4ff87988b6091e4ccbe
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-10-16 07:37:07 +0700

    Update data.ts

--- web-landing/src/components/UI/AboutUsPage/TeamAndCulture/data.ts
+++ web-landing/src/components/UI/AboutUsPage/TeamAndCulture/data.ts
@@ -1,209 +1,209 @@
-import { PersonCardProps } from "./PersonCard"
+import { PersonCardProps } from "./PersonCard";
 
 export const personLead: PersonCardProps[] = [
-    {
-        name: "Tin Nguyen",
-        job: "World Architect",
-        srcImg: "/images/avatars/1.png",
-    },
-    {
-        name: "Victor Tran",
-        job: "Co-founding Advisor",
-        srcImg: "/images/avatars/2.png",
-    },
-    {
-        name: "Loi Luu",
-        job: "Co-founding Advisor",
-        srcImg: "/images/avatars/3.png",
-    },
-]
+  {
+    name: "Tin Nguyen",
+    job: "World Architect",
+    srcImg: "/images/avatars/1.png",
+  },
+  {
+    name: "Victor Tran",
+    job: "Co-founding Advisor",
+    srcImg: "/images/avatars/2.png",
+  },
+  {
+    name: "Loi Luu",
+    job: "Co-founding Advisor",
+    srcImg: "/images/avatars/3.png",
+  },
+];
 
 export const personEmployee: PersonCardProps[] = [
-    {
-        name: "Nghia Nguyen",
-        job: "Head of Community & NFTs",
-        srcImg: "/images/avatars/4.png",
-    },
-    {
-        name: "Alain Dinh",
-        job: "Head of Partnership & B2B",
-        srcImg: "/images/avatars/5.png",
-    },
-    {
-        name: "Justin Cohen",
-        job: "Marketing Director & Marketplace",
-        srcImg: "/images/avatars/6.png",
-    },
-    {
-        name: "Joel Colon",
-        job: "Strategist",
-        srcImg: "/images/avatars/7.png",
-    },
-    {
-        name: "Duc Ho",
-        job: "Finance Manager",
-        srcImg: "/images/avatars/8.png",
-    },
-    {
-        name: "Lien Nguyen",
-        job: "Legal Counsel",
-        srcImg: "/images/avatars/9.png",
-    },
-    {
-        name: "Duyen Nguyen",
-        job: "Senior Recruiter",
-        srcImg: "/images/avatars/10.png",
-    },
-    {
-        name: "Thang Ho",
-        job: "Office Manager",
-        srcImg: "/images/avatars/11.png",
-    },
+  {
+    name: "Hai Nguyen",
+    job: "Art Director",
+    srcImg: "/images/avatars/30.png",
+  },
+  {
+    name: "Alain Dinh",
+    job: "Head of Partnership & B2B",
+    srcImg: "/images/avatars/5.png",
+  },
+  {
+    name: "Justin Cohen",
+    job: "Marketing Director & Marketplace",
+    srcImg: "/images/avatars/6.png",
+  },
+  {
+    name: "Nghia Nguyen",
+    job: "Head of Community & NFTs",
+    srcImg: "/images/avatars/4.png",
+  },
+  {
+    name: "Duc Ho",
+    job: "Finance Manager",
+    srcImg: "/images/avatars/8.png",
+  },
+  {
+    name: "Lien Nguyen",
+    job: "Legal Counsel",
+    srcImg: "/images/avatars/9.png",
+  },
+  {
+    name: "Duyen Nguyen",
+    job: "Senior Recruiter",
+    srcImg: "/images/avatars/10.png",
+  },
+  {
+    name: "Thang Ho",
+    job: "Office Manager",
+    srcImg: "/images/avatars/11.png",
+  },
 
-    {
-        name: "Quan Van",
-        job: "Frontend Developer",
-        srcImg: "/images/avatars/12.png",
-    },
-    {
-        name: "Vu Le",
-        job: "Full Stack Engineer",
-        srcImg: "/images/avatars/13.png",
-    },
-    {
-        name: "Tan Nguyen",
-        job: "Full Stack Engineer",
-        srcImg: "/images/avatars/14.png",
-    },
-    {
-        name: "Anh La",
-        job: "Full Stack Engineer",
-        srcImg: "/images/avatars/15.png",
-    },
-    {
-        name: "Dat Nguyen",
-        job: "Backend Developer",
-        srcImg: "/images/avatars/16.png",
-    },
-    {
-        name: "Son Huynh",
-        job: "Game Designer",
-        srcImg: "/images/avatars/17.png",
-    },
-    {
-        name: "Thang Nguyen",
-        job: "Game Product Lead",
-        srcImg: "/images/avatars/18.png",
-    },
-    {
-        name: "Loc Bui",
-        job: "Game Engineer Lead",
-        srcImg: "/images/avatars/19.png",
-    },
-    {
-        name: "Doan Do",
-        job: "Senior Game Engineer",
-        srcImg: "/images/avatars/20.png",
-    },
-    {
-        name: "Duy Tran",
-        job: "Game Engineer",
-        srcImg: "/images/avatars/21.png",
-    },
-    {
-        name: "Khai Huynh",
-        job: "Marketing Lead",
-        srcImg: "/images/avatars/22.png",
-    },
-    {
-        name: "Vi Truong",
-        job: "Marketing Support",
-        srcImg: "/images/avatars/23.png",
-    },
-    {
-        name: "Song Nguyen",
-        job: "Media Producer",
-        srcImg: "/images/avatars/24.png",
-    },
-    {
-        name: "Julz Zulueta",
-        job: "Marketing Executive",
-        srcImg: "/images/avatars/25.png",
-    },
-    {
-        name: "Ashraf Ahmad",
-        job: "Deputy Community Manager",
-        srcImg: "/images/avatars/26.png",
-    },
-    {
-        name: "Tram Bui",
-        job: "UI & UX Designer",
-        srcImg: "/images/avatars/27.png",
-    },
-    {
-        name: "Ai Nguyen",
-        job: "Technical Artist",
-        srcImg: "/images/avatars/28.png",
-    },
-    {
-        name: "Lam Phung",
-        job: "3D Artist Lead",
-        srcImg: "/images/avatars/29.png",
-    },
-    {
-        name: "Hai Nguyen",
-        job: "Art Director",
-        srcImg: "/images/avatars/30.png",
-    },
-    {
-        name: "Anh Tran",
-        job: "Senior 3D Artist",
-        srcImg: "/images/avatars/31.png",
-    },
-    {
-        name: "Tu Ha",
-        job: "Senior 2D Artist",
-        srcImg: "/images/avatars/32.png",
-    },
-    {
-        name: "Nhat Hoang",
-        job: "Senior 2D Concept Artist",
-        srcImg: "/images/avatars/33.png",
-    },
-    {
-        name: "Duong Nguyen",
-        job: "3D Animator",
-        srcImg: "/images/avatars/34.png",
-    },
-    {
-        name: "Vu Nguyen",
-        job: "Senior Artist",
-        srcImg: "/images/avatars/35.png",
-    },
-    {
-        name: "Khuong Nguyen",
-        job: "Junior 3D Artist",
-        srcImg: "/images/avatars/36.png",
-    },
-    {
-        name: "Manh Nghiem",
-        job: "3D Artist",
-        srcImg: "/images/avatars/37.png",
-    },
-    {
-        name: "Khue Le",
-        job: "Character Concept Artist",
-        srcImg: "/images/avatars/38.png",
-    },
-    {
-        name: "Khoa Bui",
-        job: "Senior 2D Concept Artist",
-        srcImg: "/images/avatars/39.png",
-    },
-    {
-        name: "Hung Nguyen",
-        job: "3D Artist",
-        srcImg: "/images/avatars/40.png",
-    },
-]
+  {
+    name: "Quan Van",
+    job: "Frontend Developer",
+    srcImg: "/images/avatars/12.png",
+  },
+  {
+    name: "Vu Le",
+    job: "Full Stack Engineer",
+    srcImg: "/images/avatars/13.png",
+  },
+  {
+    name: "Tan Nguyen",
+    job: "Full Stack Engineer",
+    srcImg: "/images/avatars/14.png",
+  },
+  {
+    name: "Anh La",
+    job: "Full Stack Engineer",
+    srcImg: "/images/avatars/15.png",
+  },
+  {
+    name: "Dat Nguyen",
+    job: "Backend Developer",
+    srcImg: "/images/avatars/16.png",
+  },
+  {
+    name: "Son Huynh",
+    job: "Game Designer",
+    srcImg: "/images/avatars/17.png",
+  },
+  {
+    name: "Thang Nguyen",
+    job: "Game Product Lead",
+    srcImg: "/images/avatars/18.png",
+  },
+  {
+    name: "Loc Bui",
+    job: "Game Engineer Lead",
+    srcImg: "/images/avatars/19.png",
+  },
+  {
+    name: "Doan Do",
+    job: "Senior Game Engineer",
+    srcImg: "/images/avatars/20.png",
+  },
+  {
+    name: "Duy Tran",
+    job: "Game Engineer",
+    srcImg: "/images/avatars/21.png",
+  },
+  {
+    name: "Joel Colon",
+    job: "Strategist",
+    srcImg: "/images/avatars/7.png",
+  },
+  {
+    name: "Vi Truong",
+    job: "Marketing Support",
+    srcImg: "/images/avatars/23.png",
+  },
+  {
+    name: "Khai Huynh",
+    job: "Marketing Lead",
+    srcImg: "/images/avatars/22.png",
+  },
+  {
+    name: "Song Nguyen",
+    job: "Media Producer",
+    srcImg: "/images/avatars/24.png",
+  },
+  {
+    name: "Julz Zulueta",
+    job: "Marketing Executive",
+    srcImg: "/images/avatars/25.png",
+  },
+  {
+    name: "Ashraf Ahmad",
+    job: "Deputy Community Manager",
+    srcImg: "/images/avatars/26.png",
+  },
+  {
+    name: "Tram Bui",
+    job: "UI & UX Designer",
+    srcImg: "/images/avatars/27.png",
+  },
+  {
+    name: "Ai Nguyen",
+    job: "Technical Artist",
+    srcImg: "/images/avatars/28.png",
+  },
+  {
+    name: "Lam Phung",
+    job: "3D Artist Lead",
+    srcImg: "/images/avatars/29.png",
+  },
+  {
+    name: "Anh Tran",
+    job: "Senior 3D Artist",
+    srcImg: "/images/avatars/31.png",
+  },
+  {
+    name: "Tu Ha",
+    job: "Senior 2D Artist",
+    srcImg: "/images/avatars/32.png",
+  },
+  {
+    name: "Nhat Hoang",
+    job: "Senior 2D Concept Artist",
+    srcImg: "/images/avatars/33.png",
+  },
+  {
+    name: "Duong Nguyen",
+    job: "3D Animator",
+    srcImg: "/images/avatars/34.png",
+  },
+  {
+    name: "Vu Nguyen",
+    job: "Senior Artist",
+    srcImg: "/images/avatars/35.png",
+  },
+  {
+    name: "Khuong Nguyen",
+    job: "Junior 3D Artist",
+    srcImg: "/images/avatars/36.png",
+  },
+  {
+    name: "Manh Nghiem",
+    job: "3D Artist",
+    srcImg: "/images/avatars/37.png",
+  },
+  {
+    name: "Khue Le",
+    job: "Character Concept Artist",
+    srcImg: "/images/avatars/38.png",
+  },
+  {
+    name: "Khoa Bui",
+    job: "Senior 2D Concept Artist",
+    srcImg: "/images/avatars/39.png",
+  },
+  {
+    name: "Hung Nguyen",
+    job: "3D Artist",
+    srcImg: "/images/avatars/40.png",
+  },
+];
 // comment
