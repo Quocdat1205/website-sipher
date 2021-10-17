e0376daa938867f85178df5f0e76e87f8d320a72
La Quoc Anh <anh.lq@trungthuy.vn>
2021-10-17 15:59:48 +0700

    add compression-webpack-plugin

--- web-landing/next.config.js
+++ web-landing/next.config.js
@@ -1,4 +1,14 @@
+const CompressionPlugin = require("compression-webpack-plugin")
+
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    webpack(config) {
+        config.plugins.push(
+            new CompressionPlugin({
+                test: /\.js$|\.css$|\.html$/,
+            })
+        )
+        return config
+    },
 }
