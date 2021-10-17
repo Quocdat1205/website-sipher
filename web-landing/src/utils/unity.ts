69e6304db5b2a39c5e0fdbce60dc654740fb31ca
vulh1209 <vu.le@sipher.xyz>
2021-10-17 17:51:07 +0700

    stable with no light mobile

--- web-landing/src/utils/unity.ts
+++ web-landing/src/utils/unity.ts
@@ -17,10 +17,10 @@ export const unityContext = (isMobile || isSafari)
           },
       })
     : new UnityContext({
-          loaderUrl: "/unity/mobile.loader.js",
-          dataUrl: "/unity/mobile.data",
-          frameworkUrl: "/unity/mobile.framework.js",
-          codeUrl: "/unity/mobile.wasm",
+          loaderUrl: "/unity/unity11.loader.js",
+          dataUrl: "/unity/unity11.data",
+          frameworkUrl: "/unity/unity11.framework.js",
+          codeUrl: "/unity/unity11.wasm",
           webglContextAttributes: {
               powerPreference: "low-power",
               preserveDrawingBuffer: true,
