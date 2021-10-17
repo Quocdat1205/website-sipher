12bcf9c2f767f348aef2698652b54c9227717a9a
vulh1209 <vu.le@sipher.xyz>
2021-10-17 11:27:48 +0700

    update neko mobile

--- web-landing/src/utils/unity.ts
+++ web-landing/src/utils/unity.ts
@@ -3,10 +3,10 @@ import { isMobile } from "react-device-detect"
 
 export const unityContext = isMobile
     ? new UnityContext({
-          loaderUrl: "/unity/noscroll.loader.js",
-          dataUrl: "/unity/noscroll.data",
-          frameworkUrl: "/unity/noscroll.framework.js",
-          codeUrl: "/unity/noscroll.wasm",
+          loaderUrl: "/unity/neko-mobile.loader.js",
+          dataUrl: "/unity/neko-mobile.data",
+          frameworkUrl: "/unity/neko-mobile.framework.js",
+          codeUrl: "/unity/neko-mobile.wasm",
           webglContextAttributes: {
               powerPreference: "low-power",
               preserveDrawingBuffer: true,
