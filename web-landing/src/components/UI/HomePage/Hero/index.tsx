3488d73982af6a4321059cde53c4466b6d2215db
vulh1209 <vu.le@sipher.xyz>
2021-10-17 09:06:48 +0700

    delte post processing

--- web-landing/src/components/UI/HomePage/Hero/index.tsx
+++ web-landing/src/components/UI/HomePage/Hero/index.tsx
@@ -1,5 +1,7 @@
+// 
+
 import { Box, Flex, Img } from "@chakra-ui/react";
-// import unityContext from "src/utils/unity";
+import unityContext from "src/utils/unity";
 import { useStoreActions } from "@store";
 import FirstScreen from "./FirstScreen";
 import AmountScreen from "./AmountScreen";
@@ -18,30 +20,20 @@ interface HeroProps {
 }
 
 const Hero = ({ uaString }) => {
-  // const { isIos, isIpad, isIphone, isSafari } = useUserAgent(uaString || window.navigator.userAgent);
-  // const isIOS = isIos || isIpad || isIphone || isSafari;
   const setInitialLoading = useStoreActions((action) => action.setInitialLoading);
-  //
-  // const ctnRef = useRef<HTMLDivElement>(null);
-  // const handleMouseWheel = () => {
-  //   if (ctnRef.current) unityContext.send("Main Camera", "angle", (window.scrollY / ctnRef.current.clientHeight) * 5);
-  // };
+  
+  const ctnRef = useRef<HTMLDivElement>(null);
 
-  // const handleMouseMove = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
-  //   unityContext.send("Main Camera", "effectNekoX", e.clientX / window.innerWidth);
-  // };
+  const handleMouseMove = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+    unityContext.send("Main Camera", "effectNekoX", e.clientX / window.innerWidth);
+  };
 
   useEffect(() => {
-    setInitialLoading(false);
+    unityContext.on("loaded", () => setInitialLoading(false));
   }, [setInitialLoading]);
 
-  // useEffect(() => {
-  //   window.addEventListener("scroll", handleMouseWheel);
-  //   return () => window.removeEventListener("scroll", handleMouseWheel);
-  // }, []);
-  // unityContext.on("loaded", () => setInitialLoading(false));
   return (
-    <Box pos="relative" zIndex={0} overflowX="hidden" id="hero">
+    <Box pos="relative" zIndex={0} overflowX="hidden" ref={ctnRef} id="hero">
       <Flex direction="column" w="full">
         <FirstScreen />
         <CountDownScreen />
@@ -49,14 +41,9 @@ const Hero = ({ uaString }) => {
         <PlayForJoyScreen />
         <PlayScreen />
       </Flex>
-      {/* <Box pos="fixed" top={0} left={0} h="full" w="full">
-        <Unity unityContext={unityContext} style={{ width: "100%", height: "100%" }} />
-      </Box> */}
-      <Flex align="center" justify="center" pos="fixed" top={0} left={0} h="full" w="full">
-        <div>          
-        <iframe src="https://web-necko-nine.vercel.app/" height="1080px" width="1920px"/>         
-      </div>
-      </Flex>
+        <Box pos="fixed" top={0} left={0} h="full" w="full">
+          <Unity unityContext={unityContext} style={{ width: "100%", height: "100%" }} />
+        </Box>
     </Box>
   );
 };
