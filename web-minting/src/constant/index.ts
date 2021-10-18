03eb40ad4fab7dcee2f1f857ceab16d53a2728b1
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-10-18 09:55:42 +0700

    update SC test minting

--- web-minting/src/constant/index.ts
+++ web-minting/src/constant/index.ts
@@ -3,7 +3,7 @@ export const SMARTCONTRACT_SALE_NEKO = "0x0D90B306A2ae85d428E99a5cBC801982aB1515
 // Neko contract address
 export const SMARTCONTRACT_NEKO = "0x82DdEeF2b9eeE900d6026f1841cd112442FC28C2";
 // Rinkeby chain id
-export const CHAIN_ID = "1";
+export const CHAIN_ID = "4";
 // Starting price of public sale
 export const START_PRICE = 0.9;
 // Base price of public sale
