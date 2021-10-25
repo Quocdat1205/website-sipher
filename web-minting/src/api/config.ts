f4004c1979d6b51ec918f14ff497bda66dd5a772
vulh1209 <vu.le@sipher.xyz>
2021-10-25 13:36:28 +0700

    be-sipherion

--- web-minting/src/api/config.ts
+++ web-minting/src/api/config.ts
@@ -1,4 +1,4 @@
-export const baseURL = "https://be.sipher.xyz/api/sipher/v1.1";
+export const baseURL = "https://be.sipherion.com/api/sipher/v1.1";
 // export const baseUrl = "http://localhost:5000/api/sipher/v1.0"
 
 const config = { baseURL };
