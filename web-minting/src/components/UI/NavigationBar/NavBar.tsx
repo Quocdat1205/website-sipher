8212d6a60463a668eee8d6e6e804df80b5c65ff3
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-10-28 20:01:41 +0700

    Update NavBar.tsx

--- web-minting/src/components/UI/NavigationBar/NavBar.tsx
+++ web-minting/src/components/UI/NavigationBar/NavBar.tsx
@@ -1,43 +1,44 @@
 // * DESCRIPTION:
-import { BaseNavigationBar } from "@sipher/web-components"
-import AccountAddress from "./AccountAddress"
-import useWalletContext from "@hooks/useWalletContext"
-import { Flex } from "@chakra-ui/react"
-import ChildMenu from "@components/shared/ChildMenu"
+import { BaseNavigationBar } from "@sipher/web-components";
+import AccountAddress from "./AccountAddress";
+import useWalletContext from "@hooks/useWalletContext";
+import { Flex } from "@chakra-ui/react";
+import ChildMenu from "@components/shared/ChildMenu";
 
 interface NavBarProps {
-    isInventoryMenu?: boolean
+  isInventoryMenu?: boolean;
 }
 
 export const navMenus = [
-    { id: "Public Sale", path: "/public-sale" },
-    { id: "Private Sale", path: "/private-sale" },
-    { id: "Free Minting", path: "/free-minting" },
-    { id: "Inventory", path: "/inventory/inu" },
-]
+  { id: "Public Sale", path: "/public-sale" },
+  { id: "Private Sale", path: "/private-sale" },
+  { id: "Free Minting", path: "/free-minting" },
+  { id: "Inventory", path: "/inventory/inu" },
+];
 
 export const inventoryMenus = [
-    { id: "INU", path: "/inventory/inu" },
-    { id: "NEKO", path: "/inventory/neko" },
-]
+  { id: "INU", path: "/inventory/inu" },
+  { id: "NEKO", path: "/inventory/neko" },
+];
 
 export const NavBar = ({ isInventoryMenu }: NavBarProps) => {
-    const { states } = useWalletContext()
+  const { states } = useWalletContext();
 
-    return (
-        <Flex direction="column" pos="fixed" w="full">
-            <BaseNavigationBar
-                logoPath="/images/mainlogo.svg"
-                menus={navMenus.filter(
-                    item =>
-                        item.id === "Public Sale" ||
-                        (item.id === "Private Sale" && states.whitelistInfo.privateCap > 0) ||
-                        (item.id === "Free Minting" && states.whitelistInfo.freeMintCap > 0)
-                )}
-            >
-                <AccountAddress />
-            </BaseNavigationBar>
-            {isInventoryMenu && <ChildMenu menus={inventoryMenus} />}
-        </Flex>
-    )
-}
+  return (
+    <Flex direction="column" pos="fixed" w="full">
+      <BaseNavigationBar
+        logoPath="/images/mainlogo.svg"
+        menus={navMenus.filter(
+          (item) =>
+            item.id === "Public Sale" ||
+            item.id === "Inventory" ||
+            (item.id === "Private Sale" && states.whitelistInfo.privateCap > 0) ||
+            (item.id === "Free Minting" && states.whitelistInfo.freeMintCap > 0)
+        )}
+      >
+        <AccountAddress />
+      </BaseNavigationBar>
+      {isInventoryMenu && <ChildMenu menus={inventoryMenus} />}
+    </Flex>
+  );
+};
