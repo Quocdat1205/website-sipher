797927cb3b5c80b3e10d8a1471cc38f17f79df8d
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-11-02 15:49:22 +0700

    update button minting

--- web-minting/src/components/UI/NavigationBar/AccountAddress.tsx
+++ web-minting/src/components/UI/NavigationBar/AccountAddress.tsx
@@ -1,23 +1,23 @@
-import { Box, Flex, useOutsideClick, Text, Collapse } from "@chakra-ui/react";
-import React, { useRef, useState } from "react";
-import { BiWallet } from "react-icons/bi";
-import AddressCopier from "./AddressCopier";
-import useWalletContext from "@hooks/useWalletContext";
-import { GradientButton, MyText } from "@sipher/web-components";
-import { BsChevronDown, BsChevronUp } from "react-icons/bs";
+import { Box, Flex, useOutsideClick, Text, Collapse } from "@chakra-ui/react"
+import React, { useRef, useState } from "react"
+import { BiWallet } from "react-icons/bi"
+import AddressCopier from "./AddressCopier"
+import useWalletContext from "@hooks/useWalletContext"
+import { GradientButton, MyText } from "@sipher/web-components"
+import { BsChevronDown, BsChevronUp } from "react-icons/bs"
 
 const AccountAddress = () => {
     const {
         states: { accountLogin },
         logout,
-    } = useWalletContext();
+    } = useWalletContext()
 
-    const [popup, setPopup] = useState(false);
-    const btnRef = useRef<HTMLDivElement>(null);
+    const [popup, setPopup] = useState(false)
+    const btnRef = useRef<HTMLDivElement>(null)
     useOutsideClick({
         ref: btnRef,
         handler: () => setPopup(false),
-    });
+    })
     return (
         <Flex borderLeft="1px" ml="4" pl="3" borderColor="whiteAlpha.300" flexDir="row" align="center" pos="relative">
             <Box zIndex="1" bg="black" color="main.yellow" p={[0, 1, 1.5, 2]} borderRadius="99" ref={btnRef}>
@@ -28,7 +28,7 @@ const AccountAddress = () => {
                     <MyText ml={2}>
                         {accountLogin !== "" && accountLogin.slice(0, 6)}
                         ...
-                        {accountLogin.slice(accountLogin.length - 4, accountLogin.length)}
+                        {accountLogin !== "" && accountLogin.slice(accountLogin.length - 4, accountLogin.length)}
                     </MyText>
                     <Box ml={4}>{popup ? <BsChevronUp /> : <BsChevronDown />}</Box>
                 </Flex>
@@ -45,7 +45,8 @@ const AccountAddress = () => {
                                 <Text ml={2}>
                                     {accountLogin !== "" && accountLogin.slice(0, 6)}
                                     ...
-                                    {accountLogin.slice(accountLogin.length - 4, accountLogin.length)}
+                                    {accountLogin !== "" &&
+                                        accountLogin.slice(accountLogin.length - 4, accountLogin.length)}
                                 </Text>
                                 <Box ml="auto">
                                     <AddressCopier />
@@ -58,6 +59,6 @@ const AccountAddress = () => {
                 </Box>
             </Box>
         </Flex>
-    );
-};
-export default AccountAddress;
+    )
+}
+export default AccountAddress
