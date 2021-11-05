76394a88241385995630993ef8394d30caa5c3ef
La Quoc Anh <anh.lq@trungthuy.vn>
2021-11-05 22:39:01 +0700

    update

--- web-minting/src/api/config.ts
+++ web-minting/src/api/config.ts
@@ -2,6 +2,6 @@ export const mainBaseURL = "https://be.sipher.xyz/api/sipher/v1.1"
 export const testBaseURL = "https://be.sipherion.com/api/sipher/v1.1"
 // export const baseUrl = "http://localhost:5000/api/sipher/v1.0"
 
-const config = { baseURL: mainBaseURL }
+const config = { baseURL: testBaseURL }
 
 export default config
