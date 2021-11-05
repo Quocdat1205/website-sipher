5331c457961bcb326891a29723a52cc789a2943a
La Quoc Anh <anh.lq@trungthuy.vn>
2021-11-05 16:35:48 +0700

    feat(web-minting, web-landing): not found page

--- web-landing/src/pages/404.tsx
+++ web-landing/src/pages/404.tsx
@@ -1,15 +1,59 @@
-import NotFoundUI from "@components/UI/404"
-import type { NextPage } from "next"
+import { Flex, Text } from "@chakra-ui/layout"
+import { Typo } from "@components/shared"
+import LinkButton from "@components/shared/LinkButton"
 import Head from "next/head"
-const LaboratoryPage: NextPage = () => {
+import { useRouter } from "next/router"
+interface NotfoundPageProps {}
+
+const NotfoundPage = ({}: NotfoundPageProps) => {
+    const router = useRouter()
     return (
         <>
             <Head>
-                <title>Not found</title>
+                <title>Page not found | Sipher</title>
             </Head>
-            <NotFoundUI />
+            <Flex
+                w="full"
+                h="100vh"
+                bg="black"
+                bgImage="/images/pc/home/homenew2.png"
+                align="center"
+                justify="center"
+                p={4}
+            >
+                <Flex direction="column" align="center" maxW="64rem">
+                    <Text
+                        fontFamily="Hacked"
+                        color="white"
+                        fontSize={["12rem", "15rem"]}
+                        lineHeight="12rem"
+                        letterSpacing="4px"
+                        sx={{
+                            textShadow: "-5px 0px 0px rgb(125, 255, 125), 5px 0px rgb(255, 75, 75)",
+                        }}
+                        mb={4}
+                    >
+                        404
+                    </Text>
+                    <Typo.Heading isGradient fontSize="3xl">
+                        PAGE NOT FOUND
+                    </Typo.Heading>
+                    <Typo.BoldText textAlign="center">
+                        OOPS! THE PAGE YOU'RE LOOKING FOR IS EITHER NOT AVAILABLE, LOADING INCORRECRLY, OR HAS BEEN LOST
+                        SOMEWHERE IN THE METAVERSE.
+                    </Typo.BoldText>
+                    <LinkButton
+                        text="Return Home"
+                        size="large"
+                        mt={12}
+                        px={16}
+                        onClick={() => router.push("/")}
+                        cursor="pointer"
+                    />
+                </Flex>
+            </Flex>
         </>
     )
 }
 
-export default LaboratoryPage
+export default NotfoundPage
