1080516b475ccfce89b9d900cf6f261f4093761f
La Quoc Anh <anh.lq@trungthuy.vn>
2021-11-06 13:43:48 +0700

    fix(web-minting): update title

--- web-minting/src/constant/index.ts
+++ web-minting/src/constant/index.ts
@@ -1,22 +1,17 @@
 // Sale contract address
-export const SMARTCONTRACT_SALE_NEKO = "0xa43D6b6EfBdCDa944d74bf74f87FDc8fAd1733cA"
+export const SMARTCONTRACT_SALE_NEKO = "0x39D6Fb4C116a2d41B2b7E73508Ce75326Bc8f830"
 // Neko contract address
-export const SMARTCONTRACT_NEKO = "0x9816758DdD1CD7fC87E5BEC426647f0DF42A6a1b"
+export const SMARTCONTRACT_NEKO = "0x09E0dF4aE51111CA27d6B85708CFB3f1F7cAE982"
 // Rinkeby chain id
-export const CHAIN_ID = "4"
+export const CHAIN_ID = "1"
 // Starting price of public sale
 export const START_PRICE = 0.9
 // Price step
 export const PRICE_STEP = 0.05
 // Interval: 10 MINUTES
 // Test Interval : 5 MINUTES
-export const INTERVAL = 1000 * 60 * 1
+export const INTERVAL = 1000 * 60 * 10
 // Public slot cap
 export const PUBLIC_CAP = 5
-// Price fetching interval
-export const FETCHING_INTERVAL = 1000
-// Maximum NFT public sale
-export const PUBLIC_MINTING_LIMIT = 7038
-
 // deplay 15 seconds
 export const DELAY = 1000 * 15
