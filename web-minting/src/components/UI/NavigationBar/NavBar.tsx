28acba0b32023bde315bb4c5915c2f6d751f5d97
La Quoc Anh <anh.lq@trungthuy.vn>
2021-11-08 22:56:08 +0700

    update freemint time

--- web-minting/src/components/UI/NavigationBar/NavBar.tsx
+++ web-minting/src/components/UI/NavigationBar/NavBar.tsx
@@ -32,10 +32,10 @@ export const NavBar = ({ isInventoryMenu }: NavBarProps) => {
                     item =>
                         item.id === "Public Sale 2.0" ||
                         item.id === "Inventory" ||
-                        // (item.id === "Private Sale" && states.whitelistInfo.privateCap > 0) ||
-                        // (item.id === "Free Minting" && states.whitelistInfo.freeMintCap > 0)
-                        item.id === "Private Sale" ||
-                        item.id === "Free Minting"
+                        (item.id === "Private Sale" && states.whitelistInfo.privateCap > 0) ||
+                        (item.id === "Free Minting" && states.whitelistInfo.freeMintCap > 0)
+                    // item.id === "Private Sale" ||
+                    // item.id === "Free Minting"
                 )}
             >
                 {/* <TotalSupply /> */}
