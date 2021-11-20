fbcf202bedeec7dce64871b4d7c7b5a35943043a
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-11-20 20:34:18 +0700

    Update news.ts

--- web-landing/src/hooks/api/news.ts
+++ web-landing/src/hooks/api/news.ts
@@ -2,13 +2,13 @@ import config from "./config"
 import axios from "axios"
 
 export const getListNews = async (min: number, max: number) => {
-    const { data } = await axios.get(`/communication/user?min=${min}&max=${max}`, config)
+    const { data } = await axios.get(`/communication/?min=${min}&max=${max}`, config)
 
     return data
 }
 
 export const getDetailsNews = async published => {
-    const { data } = await axios.get(`/communication/user/details?published=${published}`, config)
+    const { data } = await axios.get(`/communication/details?published=${published}`, config)
 
     return data
 }
