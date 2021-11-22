db9a59fb112560964e2b6d9de0b3407d3b43d241
La Quoc Anh <anh.lq@trungthuy.vn>
2021-11-22 20:01:14 +0700

    update navbarlink font size

--- web-landing/src/components/shared/NavBarLink.tsx
+++ web-landing/src/components/shared/NavBarLink.tsx
@@ -44,7 +44,7 @@ export const NavBarLink = ({
         >
             <Text
                 fontWeight={isChild ? "normal" : "bold"}
-                fontSize="xs"
+                fontSize={["xs", "xs", "xs", "sm", "md"]}
                 textAlign="center"
                 isTruncated
                 onClick={() => href && router.push(href)}
