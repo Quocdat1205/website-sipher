aa33f4d846380143216d2f4201f822c9f62bb13a
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-11-24 19:16:59 +0700

    Update index.tsx

--- web-landing/src/components/UI/AboutUsPage/Careers/index.tsx
+++ web-landing/src/components/UI/AboutUsPage/Careers/index.tsx
@@ -1,60 +1,60 @@
 // * DESCRIPTION:
 
-import { Flex, UnorderedList, ListItem } from "@chakra-ui/react";
-import { BackgroundContainer, LinkButton, TextContainer, Typo } from "@components/shared";
-import FlagImage from "./FlagImage";
+import { Flex, UnorderedList, ListItem } from "@chakra-ui/react"
+import { BackgroundContainer, LinkButton, TextContainer, Typo } from "@components/shared"
+import FlagImage from "./FlagImage"
 
 interface CareesProps {}
 const intro =
-  "Sipher was born from the pure passion for games and the desire to embrace blockchain technology to bring about disruptive innovation in the Gaming & Entertainment Space. We will serve our users through three main pillars:";
+    "Sipher was born from the pure passion for games and the desire to embrace blockchain technology to bring about disruptive innovation in the Gaming & Entertainment Space. We will serve our users through three main pillars:"
 const items = [
-  "A user experience focused approach based on a genuine passion for gaming and entertainment experiences.",
-  "An open and education friendly approach inclusive to users of all levels of technical knowledge, to introduce blockchain’s ethos and unique benefits.",
-  "A sustainable revenue-based model based on sound financial foundations.",
-];
+    "A user experience focused approach based on a genuine passion for gaming and entertainment experiences.",
+    "An open and education friendly approach inclusive to users of all levels of technical knowledge, to introduce blockchain’s ethos and unique benefits.",
+    "A sustainable revenue-based model based on sound financial foundations.",
+]
 const sub =
-  "Our founding team is based in Ho Chi Minh, Vietnam yet we are an international bunch with fully remote roles, with background from ";
+    "Our founding team is based in Ho Chi Minh, Vietnam yet we are an international bunch with fully remote roles, with background from "
 
 const Careers = ({}: CareesProps) => {
-  return (
-    <BackgroundContainer>
-      <Flex direction="column" align="center" px={4} py={24} w="full">
-        <TextContainer headline="CAREERS">
-          <Typo.Text>{intro}</Typo.Text>
-          <UnorderedList>
-            {items.map((item) => (
-              <ListItem key={item}>
-                <Typo.Text>{item}</Typo.Text>
-              </ListItem>
-            ))}
-          </UnorderedList>
-          <Typo.Text>
-            {sub}
-            <FlagImage srcImg="/images/flags/us.png" />
-            <FlagImage srcImg="/images/flags/vn.png" />
-            <FlagImage srcImg="/images/flags/euro.png" />
-            <FlagImage srcImg="/images/flags/indo.png" />
-            <FlagImage srcImg="/images/flags/sing.png" />
-            <FlagImage srcImg="/images/flags/rico.png" />
-            <FlagImage srcImg="/images/flags/philip.png" />
-            <FlagImage srcImg="/images/flags/germany.png" />
-            <FlagImage srcImg="/images/flags/canada.png" />
-          </Typo.Text>
-        </TextContainer>
-        <LinkButton
-          mt={4}
-          text="Join us at sipher"
-          href="https://www.notion.so/sipherhq/Join-us-at-Sipher-1357e774e38d432bb7d06078a99b43e7"
-          sx={{
-            "@media (max-width: 480px)": {
-              display: "none",
-            },
-          }}
-          size="medium"
-        />
-      </Flex>
-    </BackgroundContainer>
-  );
-};
+    return (
+        <BackgroundContainer>
+            <Flex direction="column" align="center" px={4} py={24} w="full">
+                <TextContainer headline="CAREERS">
+                    <Typo.Text>{intro}</Typo.Text>
+                    <UnorderedList>
+                        {items.map(item => (
+                            <ListItem key={item}>
+                                <Typo.Text>{item}</Typo.Text>
+                            </ListItem>
+                        ))}
+                    </UnorderedList>
+                    <Typo.Text>
+                        {sub}
+                        <FlagImage srcImg="/images/flags/us.png" />
+                        <FlagImage srcImg="/images/flags/vn.png" />
+                        <FlagImage srcImg="/images/flags/euro.png" />
+                        <FlagImage srcImg="/images/flags/indo.png" />
+                        <FlagImage srcImg="/images/flags/sing.png" />
+                        <FlagImage srcImg="/images/flags/rico.png" />
+                        <FlagImage srcImg="/images/flags/philip.png" />
+                        <FlagImage srcImg="/images/flags/germany.png" />
+                        <FlagImage srcImg="/images/flags/canada.png" />
+                    </Typo.Text>
+                </TextContainer>
+                <LinkButton
+                    mt={4}
+                    text="Join us at sipher"
+                    href="https://careers.sipher.xyz/"
+                    // sx={{
+                    //     "@media (max-width: 480px)": {
+                    //         display: "none",
+                    //     },
+                    // }}
+                    size="medium"
+                />
+            </Flex>
+        </BackgroundContainer>
+    )
+}
 
-export default Careers;
+export default Careers
