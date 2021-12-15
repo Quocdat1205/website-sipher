6e5e6092a5818f4bfe3c58bc058a602dbfe05438
Quan Van <55340595+quanvan1999@users.noreply.github.com>
2021-12-15 14:05:27 +0700

    fix inventory

---
+++ web-landing/src/components/UI/LeaderBoard/index.tsx
@@ -0,0 +1,16 @@
+// * DESCRIPTION:
+
+import { Flex, Box } from "@chakra-ui/react"
+import React from "react"
+
+const LeaderBoard = () => {
+    return (
+        <Flex direction="column" align="center" w="full">
+            <Box w="full" maxW="64rem" px={4}>
+                123
+            </Box>
+        </Flex>
+    )
+}
+
+export default LeaderBoard
