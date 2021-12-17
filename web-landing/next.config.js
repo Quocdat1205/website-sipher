642d9a1faae0595a61c01f311c78771539cadf6d
La Quoc Anh <anh.lq@trungthuy.vn>
2021-12-17 18:17:21 +0700

    update manifest

--- web-landing/next.config.js
+++ web-landing/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: "/manifest.json",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                    },
+                ],
+            },
+        ]
+    },
 }
