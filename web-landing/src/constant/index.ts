d2343c17037530df5ed26f2d2fb2065ac3a49110
vulh1209 <vu.le@sipher.xyz>
2022-01-03 16:03:28 +0700

    fix be link

--- web-landing/src/constant/index.ts
+++ web-landing/src/constant/index.ts
@@ -4,7 +4,7 @@ export const CHAIN_ID = 1;
 // export const baseURL = "https://be.sipher.xyz/api/sipher/v1.1"
 
 //test
-export const baseURL = "http://localhost:5000/api/";
+export const baseURL = "https://be.siphersion.com/api/";
 
 export const startTime = 1639544400000;
 export const TOTAL_REWARDS_FOR_POOL = 50000000; // 50 millions
