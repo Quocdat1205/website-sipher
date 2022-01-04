67037cd27094e2b0b85c3aa382a37c63db3e22db
vulh1209 <vu.le@sipher.xyz>
2022-01-04 11:18:10 +0700

    fix detail page

--- web-landing/src/components/UI/Dashboard/Inventory/Detail/useInventoryDetail.ts
+++ web-landing/src/components/UI/Dashboard/Inventory/Detail/useInventoryDetail.ts
@@ -1,64 +1,64 @@
-import { NFTRace } from "@@types"
-import { getNFT, getMerkle, changeEmotion } from "@hooks/api"
-import useWalletContext from "@hooks/web3/useWalletContext"
-import { useRouter } from "next/router"
-import { useState } from "react"
-import { useQueryClient, useQuery, useMutation } from "react-query"
+import { NFTRace } from "@@types";
+import { getNFT, getMerkle, changeEmotion } from "@hooks/api";
+import useWalletContext from "@hooks/web3/useWalletContext";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useQueryClient, useQuery, useMutation } from "react-query";
 
 const useInventoryDetail = ({ id, race }: { id: any; race: NFTRace }) => {
-    const wallet = useWalletContext()
-    const queryClient = useQueryClient()
-    const [currentEmotion, setCurrentEmotion] = useState("DEFAULT")
+  const wallet = useWalletContext();
+  const queryClient = useQueryClient();
+  const [currentEmotion, setCurrentEmotion] = useState("DEFAULT");
 
-    const router = useRouter()
+  const router = useRouter();
 
-    const { data } = useQuery(["NFT", race, id], () => getNFT({ address: wallet.account as string, id, race }), {
-        enabled: !!wallet.account && !!id,
-        onSuccess: data => {
-            setCurrentEmotion(data.emotion.toUpperCase())
-        },
-    })
+  const { data } = useQuery(["NFT", race, id], () => getNFT({ address: wallet.account as string, id, race }), {
+    enabled: !!wallet.account && !!id,
+    onSuccess: (data) => {
+      setCurrentEmotion(data.emotion.toUpperCase());
+    },
+  });
 
-    const { data: merkle } = useQuery(["merkle", race, id], () => getMerkle(parseInt(id), race), {
-        enabled: data && id,
-    })
+  const { data: merkle } = useQuery(["merkle", race, id], () => getMerkle(parseInt(id), race), {
+    enabled: !!data && !!id,
+  });
 
-    const { mutate: mutateChangeEmotion } = useMutation<unknown, unknown, string>(
-        newEmotion =>
-            changeEmotion({
-                address: wallet.account as string,
-                emotion: newEmotion,
-                id,
-                race,
-            }),
-        {
-            onSuccess: () => {
-                queryClient.invalidateQueries("NFT")
-            },
-        }
-    )
-
-    const handleDownJSON = () => {
-        const a = document.createElement("a")
-        a.href = URL.createObjectURL(new Blob([JSON.stringify(merkle)], { type: "text/json" }))
-        a.download = `Sipher${race.toUpperCase()}_#${id}.json`
-        a.click()
+  const { mutate: mutateChangeEmotion } = useMutation<unknown, unknown, string>(
+    (newEmotion) =>
+      changeEmotion({
+        address: wallet.account as string,
+        emotion: newEmotion,
+        id,
+        race,
+      }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("NFT");
+      },
     }
-    const getAvailableEmotions = () => {
-        if (!data || data.name === "???") return []
-        return data.emotions.filter(emotion => !!emotion.image)
-    }
-    const availableEmotions = getAvailableEmotions()
+  );
 
-    return {
-        data,
-        currentEmotion,
-        merkle,
-        handleDownJSON,
-        availableEmotions,
-        mutateChangeEmotion,
-        router,
-    }
-}
+  const handleDownJSON = () => {
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(new Blob([JSON.stringify(merkle)], { type: "text/json" }));
+    a.download = `Sipher${race.toUpperCase()}_#${id}.json`;
+    a.click();
+  };
+  const getAvailableEmotions = () => {
+    if (!data || data.name === "???") return [];
+    return data.emotions.filter((emotion) => !!emotion.image);
+  };
+  const availableEmotions = getAvailableEmotions();
 
-export default useInventoryDetail
+  return {
+    data,
+    currentEmotion,
+    merkle,
+    handleDownJSON,
+    availableEmotions,
+    mutateChangeEmotion,
+    router,
+  };
+};
+
+export default useInventoryDetail;
