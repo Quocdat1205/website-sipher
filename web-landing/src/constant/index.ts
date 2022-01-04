c55427bfde8d54f1388297301e0db9b0a83e19d9
vulh1209 <vu.le@sipher.xyz>
2022-01-04 09:16:22 +0700

    be sipherion

--- web-landing/src/constant/index.ts
+++ web-landing/src/constant/index.ts
@@ -4,6 +4,7 @@ export const CHAIN_ID = 1;
 // export const baseURL = "https://be.sipher.xyz/api/sipher/v1.1"
 
 //test
+// export const baseURL = "http://localhost:5000/api/";
 export const baseURL = "https://be.sipherion.com/api/";
 
 export const startTime = 1639544400000;
