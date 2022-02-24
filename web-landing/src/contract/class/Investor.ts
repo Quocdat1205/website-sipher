bfc14355b9d93201f53360a78a85d15b56bdb71f
Quan Van <vanthuanquan@gmail.com>
2022-02-24 16:33:48 +0700

    update: fix investor

--- web-landing/src/contract/class/Investor.ts
+++ web-landing/src/contract/class/Investor.ts
@@ -1,22 +1,26 @@
-import Web3 from "web3";
-import { weiToEther } from "..";
-import { InvestorAbi, InvestorAddress } from "../code";
+import Web3 from "web3"
+import { weiToEther } from ".."
+import { InvestorAbi, InvestorAddress } from "../code"
 
 export class Investor {
-    Investor;
+    Investor
 
     constructor(web3: Web3) {
-        this.Investor = new web3.eth.Contract(InvestorAbi, InvestorAddress);
+        this.Investor = new web3.eth.Contract(InvestorAbi, InvestorAddress)
     }
 
     async claim(from: string, totalAmount: string, proof: string[]) {
-        await this.Investor.methods.claim(totalAmount, proof).send({ from });
+        await this.Investor.methods.claim(totalAmount, proof).send({ from })
     }
     async claimed(from: string) {
-        return parseInt(await this.Investor.methods.claimed("0", from).call());
+        return parseInt(await this.Investor.methods.claimed("0", from).call())
     }
 
-    async getClaimableAmountAtTimestamp(from: string, totalAmount: string, proof: string[]) {
+    async getClaimableAmountAtTimestamp(
+        from: string,
+        totalAmount: string,
+        proof: string[]
+    ) {
         return weiToEther(
             await this.Investor.methods
                 .getClaimableAmountAtTimestamp(
@@ -26,6 +30,6 @@ export class Investor {
                     Math.floor(new Date().getTime() / 1000).toString()
                 )
                 .call()
-        );
+        )
     }
 }
--- web-landing/src/hooks/api/user.ts
+++ web-landing/src/hooks/api/user.ts
@@ -1,71 +1,81 @@
-import fetcher from "./fetcher";
+import fetcher from "./fetcher"
 
 export interface IUser {
-    address: string;
-    nonce: number;
+    address: string
+    nonce: number
 }
 
 /** Get user from address */
 export const getUsersByAddress = async (address: string): Promise<IUser> => {
-    const { data } = await fetcher.get(`/auth/get-nonce?publicAddress=${address}`);
+    const { data } = await fetcher.get(
+        `/auth/get-nonce?publicAddress=${address}`
+    )
     // console.log(data);
 
     return {
         nonce: data.nonce,
         address,
-    };
-};
+    }
+}
 
 /** Authenticate user by address and signature */
-export const authenticateUser = async (address: string, signature: string): Promise<boolean> => {
+export const authenticateUser = async (
+    address: string,
+    signature: string
+): Promise<boolean> => {
     const { data } = await fetcher.post(
         "/auth/gen-session",
         { publicAddress: address, signature },
         {
             withCredentials: true,
         }
-    );
-    return data;
-};
+    )
+    return data
+}
 
 export const getExpiredToken = async (address: string): Promise<boolean> => {
-    const { data } = await fetcher.get(`/auth/verify?publicAddress=${address}`, {
-        withCredentials: true,
-    });
-    return data;
-};
+    const { data } = await fetcher.get(
+        `/auth/verify?publicAddress=${address}`,
+        {
+            withCredentials: true,
+        }
+    )
+    return data
+}
 
-export const getAirdrop = async (address: string): Promise<{ totalAmount: string; proof: string[] }> => {
-    const { data } = await fetcher.get(`/airdrop/${address}/airdrops`);
+export const getAirdrop = async (
+    address: string
+): Promise<{ totalAmount: string; proof: string[] }> => {
+    const { data } = await fetcher.get(`/airdrop/${address}/airdrops`)
 
     return {
         totalAmount: data.totalAmount,
         proof: data.proof,
-    };
-};
+    }
+}
 
 export const getInvestor = async (
     address: string
 ): Promise<{
-    startTime: string;
-    vestingInterval: string;
-    totalAmount: string;
-    proof: string[];
-    numberOfVestingPoint: string;
+    startTime: string
+    vestingInterval: string
+    totalAmount: string
+    proof: string[]
+    numberOfVestingPoint: string
 }> => {
-    const { data } = await fetcher.get(`/airdrop/${address}/investors_CP1`);
+    const { data } = await fetcher.get(`/airdrop/${address}/investors_CP1`)
     return {
         startTime: data.startTime || "0",
         vestingInterval: data.vestingInterval || "0",
         totalAmount: data.totalAmount || "0",
         proof: data.proof || [],
         numberOfVestingPoint: data.numberOfVestingPoint || "0",
-    };
-};
+    }
+}
 
 export const logout = async (): Promise<boolean> => {
     const { data } = await fetcher.post("/auth/logout", {
         withCredentials: true,
-    });
-    return data;
-};
+    })
+    return data
+}
