30da491a233331a62dcf9781c51a6f280474170c
Quan Van <vanthuanquan@gmail.com>
2022-02-28 15:43:16 +0700

    Revert "update: coinbase wallet"
    This reverts commit cb07fc84fa775af0667178114623e83066e8e058.

--- web-landing/src/hooks/web3/connectors/index.ts
+++ web-landing/src/hooks/web3/connectors/index.ts
@@ -1,13 +1,11 @@
 import initGnosis from "./gnosis"
 import initInjected from "./injected"
 import initWalletConnect from "./walletConnect"
-import initCoinbaseConnect from "./coinbase"
 
 export const connectors = {
     injected: initInjected,
     walletConnect: initWalletConnect,
     gnosis: initGnosis,
-    coinbase: initCoinbaseConnect,
 } as const
 
 export type ConnectorId = keyof typeof connectors
--- web-landing/src/hooks/web3/connectors/walletConnect.ts
+++ web-landing/src/hooks/web3/connectors/walletConnect.ts
@@ -1,8 +1,5 @@
 import { CHAIN_ID } from "@constant/index"
-import {
-    UserRejectedRequestError,
-    WalletConnectConnector,
-} from "@web3-react/walletconnect-connector"
+import { UserRejectedRequestError, WalletConnectConnector } from "@web3-react/walletconnect-connector"
 import { ConnectionRejectedError } from "../errors"
 import { Connector } from "../types"
 
@@ -10,14 +7,12 @@ const initWalletConnect = (): Connector => {
     const web3ReactConnector = () => {
         return new WalletConnectConnector({
             qrcode: true,
-            infuraId: process.env.NEXT_PUBLIC_INFURA_ID_MAIN as string,
+            infuraId: "8e3937db21b341ceac1607d35ae551dd",
             supportedChainIds: [CHAIN_ID],
         })
     }
     const handleActivationError = (err: Error) => {
-        return err instanceof UserRejectedRequestError
-            ? new ConnectionRejectedError()
-            : null
+        return err instanceof UserRejectedRequestError ? new ConnectionRejectedError() : null
     }
     return {
         web3ReactConnector: web3ReactConnector(),
